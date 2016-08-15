fcdf7aaf85719301e73954aad0fd16da3afc7ff0
Paul Franz <paulkfranz@gmail.com>
2016-08-15 16:42:45 -0400

    patents google update

---
+++ patents/js/lines.js
@@ -0,0 +1,61 @@
+$(function() {
+  Highcharts.setOptions({
+    lang: {
+        decimalPoint: '.',
+        thousandsSep: ','
+    }
+});
+  $('#hcContainer').highcharts({
+    data: {
+      googleSpreadsheetKey: '1kBnEawzDO8PXQr0MRjHvm-RfVb8CezN3TZXO13uDja0',
+    },
+    chart: {
+      type: 'line',
+      backgroundColor: '#FFF',
+      border: 'none',
+      color: '#000',
+      plotShadow: true
+    },
+    credits: {
+      enabled: true,
+      href: "http://csis.org",
+      text: "CSIS China Power Project | Source: OECD"
+    },
+    title: {
+      text: "Triadic Patent Totals"
+    },
+    subtitle: {
+      text: "From 1990 to 2011"
+    },
+    legend: {
+      title: {
+        text: 'Country<br/><span style="font-size: 9px; color: #666; font-weight: normal">(Click to hide)</span>',
+        style: {
+          fontStyle: 'italic'
+        }
+      },
+      align: 'right',
+      verticalAlign: 'middle',
+      layout: 'vertical'
+    },
+    colors: ['#7ea5b2', '#303D43', '#907561', '#781F30', '#EC382A', '#61884D', '#d1e056'],
+    xAxis: {
+      tickInterval: 1,
+      tickmarkPlacement: 'on'
+    },
+    yAxis: {
+      title: {
+        text: "National currency per US dollar"
+      },
+    },
+  plotOptions:
+  {
+    line: {
+      marker: {
+        enabled: false,
+        symbol: "circle"
+      }
+    }
+  }
+});
+});
