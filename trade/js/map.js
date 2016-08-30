4ecd08a7f0081d958fc17e4349d23a6016e73352
Paul Franz <paulkfranz@gmail.com>
2016-08-30 16:12:56 -0400

    added colorAxis, updated tooltip formatter

--- trade/js/map.js
+++ trade/js/map.js
@@ -1,93 +1,143 @@
-$(function () {
+$(function() {
   Highcharts.setOptions({
-    lang: { thousandsSep: ",",
-    numericSymbols: ['K', ' M', 'B', 'T']
-  }
-});
-Highcharts.data({
-
-    googleSpreadsheetKey: '1HnRP1XCUtNM3BdLUyvWjiQd5HQDM5cGNpnshgVBMSro',
-
-    // custom handler when the spreadsheet is parsed
-    parsed: function (columns) {
-
-        // Read the columns into the data array
-        var data = [];
-        $.each(columns[0], function (i, code) {
-            data.push({
-                code: code.toUpperCase(),
-                value: parseFloat(columns[2][i]),
-                name: columns[1][i]
-            });
-        });
-    }
-// Get data and display
-$('#hcContainer').highcharts('Map', {
-
-  chart: {
-    backgroundColor: '#fff'
-  },
-  credits: {
-    enabled: true,
-    href: "http://csis.org",
-    text: "CSIS China Power Project | Source: International Monetary Fund"
-  },
-  title: {
-    text: ""
-  },
-  colorAxis: {
-    type: 'logarithmic',
-    maxColor: '#4E6D78',
-    min: 6000,
-    max: 590000000000
-  },
-  mapNavigation: {
-    enabled: true,
-    enableMouseWheelZoom:false,
-    buttonOptions: {
-      verticalAlign: 'bottom'
-    }
-  },
-  series: [{
-    animation: false,
-    // data: dataObj.data,
-    tooltip: {
-      headerFormat: "<span style='font-size: 12px'>{point.key}</span><br/>",
-      pointFormat:  "<b>${point.value:,.0f}</b><br/>"
+    lang: {
+      thousandsSep: ",",
+      numericSymbols: ['K', ' M', 'B', 'T']
     },
-    mapData: Highcharts.maps["custom/world"],
-    joinBy: [ 'iso-a3', 'iso-a3'],
-    borderColor: 'rgb(61, 60, 61)',
-    borderWidth: 0.2,
-
-    states: {
-      hover: {
-        color: '#ED392A'
+    chart: {
+      height: 700,
+      style: {
+        fontFamily: 'Roboto'
       }
     }
-    }],
-    motion: {
-      enabled: true,
-      // labels: dataObj.labels,
-      series: 0,
-      axisLabel: 'year',
-      magnet: {
-        round: 'floor', // ceil / floor / round
-        step: 0.1
+  });
+
+  var googleSpreadsheetKey = '1HnRP1XCUtNM3BdLUyvWjiQd5HQDM5cGNpnshgVBMSro',
+  data = [],
+  labels = [];
+
+  $.ajax({
+    dataType: 'json',
+    url: 'https://spreadsheets.google.com/feeds/cells/' +
+    googleSpreadsheetKey + '/' + 'od6' +
+    '/public/values?alt=json-in-script&callback=?',
+    success: function(json) {
+      var cells = json.feed.entry,
+      cell,
+      cellValue,
+      cellCount = cells.length,
+      colCount = 27,
+      rowCount = 8,
+      row,
+      col,
+      i,
+      point,
+      sequence;
+
+      for (i = 2; i <= colCount; i++) {
+        labels.push(+cells[i].content.$t)
       }
-    },
-    exporting: {
-      buttons: {
-        contextButton: {
-          enabled: false
-        },
-        printButton: {
-          text: 'Export Map State',
-          onclick: function () {
-            this.print();
+
+      for (i = colCount; i < cellCount; i++) {
+        cell = cells[i],
+        col = +cell.gs$cell.col;
+        value = cell.content.$t;
+
+        if (col === 1) {
+          point = {};
+          sequence = [];
+          point.country = value;
+        } else if (col === 2) {
+          point['iso-a3'] = value;
+        } else if (3 <= col && col <= colCount) {
+          sequence.push(+value);
+          if (col === colCount) {
+            point.sequence = sequence;
+            point.value = null;
+            data.push(point);
           }
         }
       }
+      data.push({
+                    'iso-a3': 'CHN',
+                    value: null,
+                    color: '#ED392A'
+                });
+
+      // Initiate the chart
+      $('#container').highcharts('Map', {
+
+        title: {
+          text: 'Global Trade with China'
+        },
+        subtitle: {
+          text: 'Total export/imports from 1990 - 2014'
+        },
+        credits: {
+          text: 'CSIS/China Power Project',
+          href: 'https://chinapower.csis.org'
+        },
+
+        mapNavigation: {
+          enabled: true,
+          buttonOptions: {
+            verticalAlign: 'bottom'
+          }
+        },
+
+        motion: {
+          enabled: true,
+          axisLabel: 'year',
+          labels: labels,
+          series: 0, // The series which holds points to update
+          updateInterval: 1,
+          magnet: {
+            round: 'floor', // ceil / floor / round
+            step: 0.1
+          }
+        },
+
+        colorAxis: {
+          stops: [
+            [0, '#1478E6'],
+            [0.1, '#1884D5'],
+            [0.2, '#1D90C4'],
+            [0.3, '#219CB4'],
+            [0.4, '#26A8A3'],
+            [0.5, '#2AB592'],
+            [0.6, '#2FC182'],
+            [0.7, '#33CD71'],
+            [0.8, '#38D960'],
+            [0.9, '#3DE650']
+          ],
+          min: 2000,
+          max: 590000000000
+        },
+
+        tooltip: {
+          formatter: function () {
+            if (this.point.value == 0) {
+              return '<p><em>Data not currently available for this year.</em></p>'
+            } else {
+              return '<p><b>'+ this.point.name + ' | ' + this.series.label + '</b></p><br />$' + Highcharts.numberFormat(this.point.value,0,',',',') + '</p>';
+            }
+          }
+        },
+
+        series: [{
+          data: data,
+          mapData: Highcharts.maps['custom/world-palestine-highres'],
+          joinBy: 'iso-a3',
+          states: {
+            hover: {
+              color: '#BADA55'
+            }
+          },
+          dataLabels: {
+            enabled: false
+          }
+        }]
+      });
     }
   });
 });
