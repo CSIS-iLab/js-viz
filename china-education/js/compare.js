ccca01984ab857dfdd755edf6bfcfc464312d2b4
Jacque Schrag <jnschrag@gmail.com>
2016-11-15 16:16:15 -0500

    Update interactive styling

--- china-education/js/compare.js
+++ china-education/js/compare.js
@@ -8,15 +8,12 @@ $(function () {
 			style: {
                 fontFamily: 'Roboto Slab'
             },
-            marginTop: 120,
+            marginTop: 20,
             plotBorderColor: "#ECECEC",
             plotBorderWidth: 1
         },
         title: {
-            text: 'Educational Attainment by Province'
-        },
-        subtitle: {
-        	text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            text: null
         },
         credits: {
 			enabled: true,
