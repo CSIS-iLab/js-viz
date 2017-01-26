db92f66c64fc8199d1e33809c407466827dcdff5
Jacque Schrag <jnschrag@gmail.com>
2017-01-26 14:08:30 -0500

    CPP FS: Created 2 line charts

---
+++ china-food-security/js/lines1.js
@@ -0,0 +1,90 @@
+$(function() {
+  Highcharts.setOptions({
+    lang: {
+        decimalPoint: '.',
+        thousandsSep: ','
+    }
+});
+  $('#hcContainer').highcharts({
+    data: {
+      googleSpreadsheetKey: '1xtUO5EOvcM2LI_MWggy1VokGpAy00PTJH2EvUiW7h-Y',
+    },
+    chart: {
+      type: 'line',
+      backgroundColor: '#FFF',
+      border: 'none',
+      color: '#000',
+      plotShadow: true,
+      style: {
+        fontFamily: 'Roboto Slab'
+      }
+    },
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS China Power Project | UNDP, World Bank",
+      style: {
+        fontFamily: 'Roboto Slab'
+      }
+    },
+    title: {
+      text: "Lines 1",
+      style: {
+        fontFamily: 'Roboto Slab'
+      }
+    },
+    subtitle: {
+      text: "2005-2014",
+      style: {
+        fontFamily: 'Roboto Slab'
+      }
+    },
+    legend: {
+      title: {
+        text: 'Income Level<br/><span style="font-size: 9px; color: #666; font-weight: normal">(Click to hide)</span>',
+        style: {
+          fontStyle: 'italic',
+          fontFamily: 'Roboto Slab'
+        }
+      },
+      align: 'right',
+      verticalAlign: 'middle',
+      layout: 'vertical'
+    },
+  colors: ['#ffd160', '#aa142d', '#ef9365', '#303d43', '#9864b6', '#5aa993', '#ed392a'],
+    xAxis: {
+      tickInterval: 1,
+      tickmarkPlacement: 'on'
+    },
+    yAxis: {
+      title: {
+        text: "Education Index",
+        style: {
+          fontFamily: 'Roboto Slab'
+        }
+      },
+    },
+  plotOptions:
+  {
+    line: {
+      marker: {
+        enabled: false,
+        symbol: "circle"
+      }
+    }
+  },
+  responsive: {
+      rules: [{
+          condition: {
+              maxWidth: 500
+          },
+          chartOptions: {
+              legend: {
+                  align: 'center',
+                  verticalAlign: 'bottom'
+              }
+          }
+      }]
+  }
+});
+});
