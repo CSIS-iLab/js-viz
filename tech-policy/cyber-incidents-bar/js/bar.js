94f17f29915b19ba563c5925f402e16c61ed0b10
Leeza <leeza@iLabs-iMac.local>
2018-08-07 16:58:22 -0400

    Add Significant Cyber Incidents Chart

---
+++ tech-policy/cyber-incidents-bar/js/bar.js
@@ -0,0 +1,121 @@
+$(function() {
+    var chart, dataArray, newOffset
+    var max, data = {}
+
+    Highcharts.data({
+        googleSpreadsheetKey: '1fdRF457KDaedi5pTe852vJD1RCEfWJryhOfz8HAa4ww',
+        googleSpreadsheetWorksheet: 1,
+        switchRowsAndColumns: false,
+        parsed: function(columns) {
+            data.nations = columns[0].slice(1)
+            data.offender = columns[1].slice(1)
+            data.victim = columns[2].slice(1)
+            renderChart(data)
+
+            let containerWidth = document.getElementById("hcContainer").offsetWidth
+            let adjustedOffset = 0 - containerWidth / 2
+            chart.update({
+                xAxis: {
+                    offset: adjustedOffset
+                }
+            })
+        }
+    })
+
+
+    window.addEventListener('resize', () => {
+        let containerWidth = document.getElementById("hcContainer").offsetWidth
+        let adjustedOffset = 0 - containerWidth / 2
+        chart.update({
+            xAxis: {
+                offset: adjustedOffset
+            }
+        })
+
+    })
+
+    function renderChart(data) {
+        return chart = Highcharts.chart('hcContainer', {
+            // $('#hcContainer').highcharts({
+            chart: {
+                margin: [100, 50, 100, 50],
+                type: 'bar'
+            },
+            legend: {
+                align: 'center',
+                verticalAlign: 'bottom',
+                layout: 'horizontal',
+                y: 5,
+                itemHoverStyle: {
+                    color: "#000",
+                    cursor: "default"
+                }
+            },
+            title: {
+                text: "Significant Cyber Incidents"
+            },
+            // Credits
+            credits: {
+                enabled: true,
+                href: false,
+                text: "CSIS Technology Policy | Cybersecurity & Governance"
+            },
+            tooltip: {
+                // shared: true,
+                crosshairs: true,
+                borderColor: Highcharts.getOptions().colors[2]
+            },
+            plotOptions: {
+                series: {
+                    events: {
+                        legendItemClick: function() {
+                            return false;
+                        }
+                    }
+                }
+            },
+            xAxis: {
+                title: {
+                    text: null
+                },
+                categories: data.nations,
+                labels: {
+                    align: "center",
+                    style: {
+                        width: "100%",
+                        fontWeight: "bold"
+                    }
+                },
+                lineWidth: 0,
+                tickWidth: 0,
+                offset: -600
+            },
+            yAxis: [{
+                title: {
+                    text: null
+                },
+                width: "40%",
+                reversed: true,
+                opposite: false
+            }, {
+                offset: 0,
+                title: {
+                    text: null
+                },
+                left: "60%",
+                width: "40%"
+            }],
+            series: [{
+                name: 'Offender',
+                yAxis: 0,
+                data: data.offender
+            }, {
+                name: 'Victim',
+                yAxis: 1,
+                data: data.victim
+
+            }]
+        });
+    }
+
+});
---
+++ tech-policy/cyber-incidents-bar/js/hc-tp-basic-theme.js
@@ -0,0 +1,111 @@
+/**
+ * CSIS Technology Policy Highcharts Theme
+ *
+ */
+
+Highcharts.setOptions({
+    lang: {
+        thousandsSep: ","
+    }
+});
+
+Highcharts.theme = {
+  colors: ['#0065a4', '#0a8672', '#115175', '#0faa91', '#004165', '#e5eff6', '#0095AB', '#66c6cb', '#566377', '#51746d'],
+    chart: {
+        backgroundColor: '#FFF',
+        border: 'none',
+        color: '#000',
+        plotShadow: false,
+        height: 500
+    },
+    title: {
+        style: {
+            color: '#000',
+            font: '25px "Roboto", Arial, sans-serif',
+            fontWeight: '400'
+        },
+        widthAdjust: -60
+    },
+    subtitle: {
+        style: {
+            fontSize: '12px',
+            fontFamily: "'Hind', 'Arial', sans-serif",
+            color: '#808080'
+        }
+    },
+    credits: {
+        style: {
+            cursor: "default",
+            fontFamily: "'Hind', 'Arial', sans-serif"
+        }
+    },
+    tooltip: {
+        style: {
+            fontSize: '13px',
+            fontFamily: "'Hind', 'Arial', sans-serif"
+        },
+        headerFormat: '<span style="font-size: 13px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: \'Roboto\', arial, sans-serif;">{point.key}</span><br/>'
+    },
+    xAxis: {
+        labels: {
+            style: {
+                color: '#666',
+                fontSize: '12px',
+                fontFamily: "'Hind', 'Arial', sans-serif"
+            },
+        },
+        title: {
+            style: {
+                color: '#666',
+                fontSize: '13px',
+                fontFamily: "'Hind', 'Arial', sans-serif"
+
+            }
+        },
+        gridLineWidth: 1,
+        lineWidth: 0,
+        tickColor: '#e6e6e6'
+    },
+    yAxis: {
+        labels: {
+            style: {
+                color: '#666',
+                fontSize: '12px',
+                fontFamily: "'Hind', 'Arial', sans-serif"
+            },
+            x: -3
+        },
+        title: {
+            style: {
+                color: '#666',
+                fontSize: '13px',
+                fontFamily: "'Hind', 'Arial', sans-serif"
+            },
+            margin: 20
+        },
+        tickColor: '#e6e6e6'
+    },
+    legend: {
+        title: {
+            style: {
+                fontFamily: "'Roboto', 'Arial', sans-serif",
+                fontSize: "15px",
+                color: '#000',
+                fontStyle: 'normal'
+            }
+        },
+        itemStyle: {
+            color: '#000',
+            fontSize: '14px',
+            fontFamily: "'Hind', 'Arial', sans-serif",
+            textOverflow: null
+
+        },
+        itemHoverStyle: {
+            color: '#ed392a'
+        }
+    }
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
