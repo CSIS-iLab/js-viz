c7e8114d8d9e070164dff3f56d7bbe53f1cb5a6d
Leeza <leeza@iLabs-iMac.local>
2018-08-08 15:00:26 -0400

    Sort and other edits

--- tech-policy/cyber-incidents-bar/js/bar.js
+++ tech-policy/cyber-incidents-bar/js/bar.js
@@ -1,13 +1,28 @@
 $(function() {
-  var chart, dataArray, newOffset;
-  var max,
-    data = {};
+  var chart, newOffset;
+  var data = [];
 
   Highcharts.data({
     googleSpreadsheetKey: "1fdRF457KDaedi5pTe852vJD1RCEfWJryhOfz8HAa4ww",
     googleSpreadsheetWorksheet: 1,
     switchRowsAndColumns: false,
     parsed: function(columns) {
+      columns[0].slice(1).forEach((column, i) => {
+        nation = {};
+        nation[column] = {};
+        nation[column].offender = columns[1].slice(1)[i];
+        nation[column].victim = columns[2].slice(1)[i];
+        nation[column].incidents =
+          nation[column].offender + nation[column].victim;
+        data.push(nation);
+      });
+
+      data = data.sort((b, a) => {
+        let nationA = Object.keys(a)[0];
+        let nationB = Object.keys(b)[0];
+        return a[nationA].incidents - b[nationB].incidents;
+      });
+
       data.nations = columns[0].slice(1);
       data.offender = columns[1].slice(1);
       data.victim = columns[2].slice(1);
@@ -41,33 +56,34 @@ $(function() {
         align: "center",
         verticalAlign: "bottom",
         layout: "horizontal",
-        y: 5,
-        itemHoverStyle: {
-          color: "#000",
-          cursor: "default"
-        }
+        y: 5
       },
       title: {
         text: "Significant Cyber Incidents"
+      },
+      subtitle: {
+        text:
+          "The above charts are based on publicly reported information describing acts of state-sponsored cyber espionage and cyber warfare, and do not include instances of cyber crime. Long-running campaigns were treated as single instances for the purposes of incident totals."
       },
       // Credits
       credits: {
         enabled: true,
         href: false,
-        text: "CSIS Technology Policy | Cybersecurity & Governance"
+        text: "CSIS Technology Policy | SOURCE"
       },
       tooltip: {
-        // shared: true,
         crosshairs: true,
-        borderColor: Highcharts.getOptions().colors[2]
+        borderColor: Highcharts.getOptions().colors[2],
+
+        valueSuffix: " incidents"
       },
       plotOptions: {
         series: {
-          events: {
-            legendItemClick: function() {
-              return false;
-            }
-          }
+          // events: {
+          //   legendItemClick: function() {
+          //     return false;
+          //   }
+          // }
         }
       },
       xAxis: {
@@ -79,7 +95,7 @@ $(function() {
           align: "center",
           style: {
             width: "100%",
-            fontWeight: "bold"
+            color: "black"
           }
         },
         lineWidth: 0,
@@ -103,6 +119,11 @@ $(function() {
           max: data.max,
           left: "60%",
           width: "40%"
+        },
+        {
+          title: {
+            text: "Number of Incidents"
+          }
         }
       ],
       series: [
