96cbbcb17657bfdfcf41d29e6b47c489d3470713
leezaweb <leeza.web@gmail.com>
2018-12-27 14:29:52 -0500

    start surveillance timeline map

---
+++ ghpc_bubble-timeline/js/map.js
@@ -0,0 +1,258 @@
+let dataObj = { data: [], labels: [] };
+
+let geoData, chart;
+
+fetch(
+  "https://raw.githubusercontent.com/mustafasaifee42/Tile-Grid-Map/master/Tile-Grid-Map-Cleaned.json"
+)
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(json) {
+    geoData = json;
+
+    Highcharts.data({
+      googleSpreadsheetKey: "12_yhWuslrui9_kW57-HwySPk9kv1Mp2VlAYHUo5QWO8",
+      googleSpreadsheetWorksheet: 2,
+      switchRowsAndColumns: true,
+      parsed: function(columns) {
+        columns.forEach((code, i) => {
+          if ([0, 1, 2].includes(i)) {
+            return;
+          }
+
+          if (i === 3) {
+            dataObj.labels = code;
+            return;
+          }
+
+          dataObj.labels.forEach((year, index) => {
+            if (index === 0) return;
+            if (index !== 0) {
+              let tileData = geoData.find(country => country.name === code[0]);
+
+              if (!tileData) return;
+
+              let countryData = dataObj.data.find(d => d.name === code[0]);
+
+              if (countryData) {
+                countryData.sequence.push({
+                  year: year,
+                  value: code[index]
+                });
+
+                countryData.value =
+                  countryData.sequence[countryData.sequence.length - 1].value;
+
+                countryData.x = countryData.coordinates[0];
+                countryData.y = countryData.coordinates[1];
+                countryData.y = Math.abs(countryData.y - 20);
+              } else {
+                let country = Object.assign({}, tileData);
+                country.sequence = country.sequence || [];
+                country.sequence.push({ year: year, value: code[index] });
+                dataObj.data.push(country);
+              }
+            }
+          });
+        });
+
+        dataObj.labels = dataObj.labels.slice(1);
+        renderChart(dataObj);
+      }
+    });
+  });
+function renderChart(data) {
+  chart = Highcharts.chart("container", {
+    chart: {
+      type: "tilemap",
+      height: "80%",
+      marginBottom: 30
+    },
+
+    title: {
+      align: "left",
+      x: 50,
+      text: "Surveillance Scores by year and country"
+    },
+
+    subtitle: {
+      widthAdjust: -150,
+      align: "left",
+      x: 50,
+      text:
+        "A States Parties Questionnaire (also referred to as the IHR monitoring questionnaire) is sent annually to National IHR Focal Points (NFPs) for data collection. It contains a checklist of 20 indicators specifically developed for monitoring each core capacity, including its status of implementation."
+    },
+
+    xAxis: {
+      visible: false
+    },
+
+    yAxis: {
+      visible: false
+    },
+    credits: {
+      enabled: true,
+      href: true,
+      position: { y: -10 },
+      text: `CSIS Global Health Policy Center | Source: <a href=http://apps.who.int/gho/data/view.main.IHRCTRY03v?lang=en">World Health Organization</a>`
+    },
+    legend: {
+      enabled: true,
+      layout: "horizontal",
+      align: "bottom",
+      y: 10,
+      floating: false
+    },
+
+    colorAxis: {
+      dataClasses: [
+        {
+          to: 25,
+          color: "#90b2dc",
+          name: "< 25"
+        },
+        {
+          from: 25,
+          to: 50,
+          color: "#6190C9",
+          name: "> 25"
+        },
+        {
+          from: 50,
+          to: 75,
+          color: "#305F98",
+          name: "> 50"
+        },
+        {
+          from: 75,
+          color: "#234670",
+          name: "> 75"
+        }
+      ]
+    },
+
+    plotOptions: {
+      series: {
+        tileShape: "circle",
+        dataLabels: {
+          enabled: true,
+          overflow: false,
+          formatter: function(point) {
+            let color;
+
+            switch (this.color) {
+              case "#234670":
+                color = "#90b2dc";
+                break;
+              case "#305F98":
+                color = "#6190C9";
+                break;
+              case "#6190C9":
+                color = "#305F98";
+                break;
+              case "#90b2dc":
+                color = "#234670";
+                break;
+              default:
+                return;
+            }
+
+            return `<span style="font-size:font-size: .5rem;color:${color}">${
+              this.point["alpha-2"]
+            }</span>`;
+          },
+          color: "#000000",
+          style: {
+            textOutline: false
+          }
+        },
+        point: {
+          events: {
+            mouseOver: function() {
+              this.series.points
+                .filter(p => p.region === this.region)
+                .forEach(p => p.setState("hover"));
+            },
+            mouseOut: function() {
+              this.series.points
+                .filter(p => p.region === this.region)
+                .forEach(p => p.setState(""));
+            }
+          }
+        }
+      }
+    },
+
+    series: [
+      {
+        data: data.data,
+        states: {
+          hover: {
+            brightness: 0.125
+          }
+        }
+      }
+    ],
+    mapNavigation: {
+      enabled: true,
+      enableMouseWheelZoom: false,
+      buttonOptions: {
+        verticalAlign: "top"
+      }
+    },
+    exporting: {
+      enabled: true
+    },
+    tooltip: {
+      headerFormat: "",
+      pointFormatter: function() {
+        let year = document.querySelector("#play-output").innerText;
+        let score = this.value;
+
+        return `<div><span style="font-size:18px;color:${
+          this.color
+        }">● </span><b>${this.name}</b><br/>
+      ${year} Score:
+       ${score}</div>`;
+      }
+    },
+
+    motion: {
+      enabled: true,
+      labels: data.labels,
+      series: 0,
+      updateInterval: 500,
+      axisLabel: "year",
+      magnet: {
+        round: "floor", // ceil / floor / round
+        step: 1
+      }
+    },
+    navigation: {
+      buttonOptions: {
+        theme: {
+          // Good old text links
+          style: {
+            color: "#039",
+            textDecoration: "underline"
+          }
+        }
+      }
+    },
+    exporting: {
+      buttons: {
+        contextButton: {
+          enabled: false
+        },
+        printButton: {
+          text: "Export Map",
+          onclick: function() {
+            this.print();
+          }
+        }
+      }
+    }
+  });
+  chart.motion.reset();
+}
