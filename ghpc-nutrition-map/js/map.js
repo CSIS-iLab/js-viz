c59bee454a53aecfeb013a060001643fa461746f
leezaweb <leeza.web@gmail.com>
2019-03-11 12:54:07 -0400

    start triple burden map

--- ghpc-nutrition-map/js/map.js
+++ ghpc-nutrition-map/js/map.js
@@ -12,16 +12,13 @@ function _defineProperty(obj, key, value) {
   return obj;
 }
 
-var clicked = false,
-  series = [];
-
 var dataObj = {
   Overweight: { data: [] },
   Anaemia: { data: [] },
   Stunting: { data: [] }
 };
 
-var geoData, currentSeries, currentPoint, currentYear, currentIndex, chart, max;
+var geoData, chart;
 
 fetch("https://code.highcharts.com/mapdata/custom/world-eckert3.geo.json")
   .then(function(resp) {
@@ -59,27 +56,110 @@ fetch("https://code.highcharts.com/mapdata/custom/world-eckert3.geo.json")
                 dataObj[key].data.push(country);
               }
             });
-
-            // switch(true){
-            //
-            //   case code[2].toLowerCase().indexOf( "overweight") > -1:
-            //     dataObj["Overweight"].data.push(country)
-            //   case code[2].toLowerCase().indexOf( "anaemia") > -1:
-            //     dataObj["Anaemia"].data.push(country)
-            //   case code[2].toLowerCase().indexOf( "stunting") > -1:
-            //     dataObj["Stunting"].data.push(country)
-            // }
           }
         });
 
-        console.log(dataObj);
+        var patternArray = [
+          "M 0 10 L 10 0 M -1 1 L 1 -1 M 9 11 L 11 9",
+          "M 0 0 L 10 10 M 9 -1 L 11 1 M -1 9 L 1 11",
+          "M 3 0 L 3 10 M 8 0 L 8 10",
+          "M 0 3 L 10 3 M 0 8 L 10 8",
+          "M 0 3 L 5 3 L 5 0 M 5 10 L 5 7 L 10 7",
+          "M 3 3 L 8 3 L 8 8 L 3 8 Z",
+          "M 5 5 m -4 0 a 4 4 0 1 1 8 0 a 4 4 0 1 1 -8 0",
+          "M 10 3 L 5 3 L 5 0 M 5 10 L 5 7 L 0 7",
+          "M 2 5 L 5 2 L 8 5 L 5 8 Z;M 0 0 L 5 10 L 10 0"
+        ];
+
+        var colorArray = ["#75baa9", "#eda27c", "#3e86b3"];
+        var disabledSvg =
+          '<svg xmlns="http://www.w3.org/2000/svg"><circle cx="6" cy="6" r="5" stroke="#bcbcbc" fill="#bcbcbc"/></svg>';
+
+        dataObj = Object.keys(dataObj).map(function(key, index) {
+          var legendItems = [...document.querySelectorAll(".legend li")].slice(
+            1
+          );
+
+          var enabledSvg = `<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"> <path d="${
+            patternArray[index]
+          }" stroke="${
+            colorArray[index]
+          }" fill="transparent" stroke-width="2" /></svg>`;
+
+          var icon = legendItems[index].querySelector("span");
+
+          icon.style.backgroundImage = `url('${"data:image/svg+xml;base64," +
+            window.btoa(enabledSvg)}')`;
+
+          legendItems[index].addEventListener("click", function(item) {
+            var series = chart.series.find(function(s) {
+              return s.name === item.target.innerText;
+            });
+
+            if (series.visible) {
+              legendItems[index].style.color = "#bcbcbc";
+              icon.style.backgroundSize = "cover";
+              icon.style.backgroundImage = `url('${"data:image/svg+xml;base64," +
+                window.btoa(disabledSvg)}')`;
+            } else {
+              legendItems[index].style.color = "black";
+              icon.style.backgroundSize = "67%";
+              icon.style.backgroundImage = `url('${"data:image/svg+xml;base64," +
+                window.btoa(enabledSvg)}')`;
+            }
+
+            series.update(
+              {
+                visible: !series.visible
+              },
+              true
+            );
+          });
+
+          return {
+            ...dataObj[key],
+
+            name: key,
+            mapData: Highcharts.maps["custom/world-eckert3"],
+            joinBy: ["hc-key", "hc-key"],
+            states: {
+              hover: {
+                brightness: 0.125
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            color: {
+              pattern: {
+                color: colorArray[index],
+                path: {
+                  d: patternArray[index]
+                },
+                width: 10,
+                height: 10,
+                opacity: 1
+              }
+            },
+            borderColor: "#bcbcbc",
+            borderWidth: 1,
+            nullColor: "transparent",
+
+            states: {
+              hover: {
+                borderColor: "white",
+                borderWidth: 2
+              }
+            }
+          };
+        });
 
         renderMap(dataObj);
       }
     });
   });
 
-function renderMap(data) {
+function renderMap(series) {
   chart = Highcharts.mapChart(
     "container",
     _defineProperty({
@@ -95,84 +175,14 @@ function renderMap(data) {
         href: true
       },
       legend: {
-        enabled: true,
+        enabled: false,
         layout: "horizontal",
         verticalAlign: "top",
         floating: false,
         x: -133
       },
 
-      series: [
-        {
-          data: data.data,
-          mapData: Highcharts.maps["custom/world-eckert3"],
-          joinBy: ["hc-key", "hc-key"],
-          states: {
-            hover: {
-              brightness: 0.125
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          color: "url(#highcharts-default-pattern-0",
-          borderColor: "#bcbcbc",
-          borderWidth: 1,
-
-          states: {
-            hover: {
-              borderColor: "white",
-              borderWidth: 2
-            }
-          }
-        },
-        {
-          data: data.data,
-          mapData: Highcharts.maps["custom/world-eckert3"],
-          joinBy: ["hc-key", "hc-key"],
-          states: {
-            hover: {
-              brightness: 0.125
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          color: "url(#highcharts-default-pattern-2",
-          borderColor: "#bcbcbc",
-          borderWidth: 1,
-
-          states: {
-            hover: {
-              borderColor: "white",
-              borderWidth: 2
-            }
-          }
-        },
-        {
-          data: data.data,
-          mapData: Highcharts.maps["custom/world-eckert3"],
-          joinBy: ["hc-key", "hc-key"],
-          states: {
-            hover: {
-              brightness: 0.125
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          color: "url(#highcharts-default-pattern-3",
-          borderColor: "#bcbcbc",
-          borderWidth: 1,
-
-          states: {
-            hover: {
-              borderColor: "white",
-              borderWidth: 2
-            }
-          }
-        }
-      ],
+      series: series,
       mapNavigation: {
         enabled: true,
         enableMouseWheelZoom: false,
