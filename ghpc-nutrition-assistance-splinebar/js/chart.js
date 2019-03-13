ec618f18617e05e43f47dfd8401cf5d9ff4e7da5
leezaweb <leeza.web@gmail.com>
2019-03-13 13:31:39 -0400

    fix

--- ghpc-nutrition-assistance-splinebar/js/chart.js
+++ ghpc-nutrition-assistance-splinebar/js/chart.js
@@ -1,8 +1,9 @@
 var allSeries = {},
   legend = {
+    funding: "Funding",
     vitamin: "Children provided with vitamin A supplements",
-    programs:
-      "Children rached by nutrition programs or nutrition-specific-interventions",
+    combined:
+      "Children reached by nutrition programs or nutrition-specific-interventions",
     anemia: " of children have anemia",
     women: " of women with anemia"
   },
@@ -17,21 +18,21 @@ var allSeries = {},
     Mali: " (2012–2013)",
     Ghana: " (2014)",
     Ethiopia: " (2016)"
-  },
-  colorArray = ["#0a8672", "#0065a4", "#666"];
-
+  };
 Highcharts.data({
   googleSpreadsheetKey: "1RXsxwg_tns3CICc1ZyYX3PEucq_RVMPDihn2y1Xs5jk",
-  googleSpreadsheetWorksheet: 8,
+  googleSpreadsheetWorksheet: 1,
   complete: function(data) {
+    var fundingSeries;
+
     var countrySeries = data.series.find(function(series) {
       return series.name === "country";
     });
 
-    countrySeries.data.forEach(function(country, index) {
+    countrySeries.data.forEach(function(country, x) {
       allSeries[country[1]] = allSeries[country[1]] || {};
 
-      var fundingSeries = data.series.find(function(series) {
+      fundingSeries = data.series.find(function(series) {
         return series.name === "funding";
       });
 
@@ -45,44 +46,18 @@ Highcharts.data({
         allSeries[country[1]][metric].series =
           allSeries[country[1]][metric].series || [];
 
+        allSeries[country[1]][metric] = allSeries[country[1]][metric] || {};
+
         allSeries[country[1]][metric].series[0] =
           allSeries[country[1]][metric].series[0] || {};
-
-        allSeries[country[1]][metric].series[0].name = "funding";
-
-        allSeries[country[1]][metric].series[0].yAxis = 0;
 
         allSeries[country[1]][metric].series[0].data =
           allSeries[country[1]][metric].series[0].data || [];
 
         allSeries[country[1]][metric].series[0].data.push(
-          fundingSeries.data[index]
-        );
-
-        allSeries[country[1]][metric] = allSeries[country[1]][metric] || {};
-
-        allSeries[country[1]][metric].series[1] =
-          allSeries[country[1]][metric].series[1] || {};
-
-        allSeries[country[1]][metric].series[1].name = metric;
-
-        allSeries[country[1]][metric].series[1].yAxis = 1;
-
-        allSeries[country[1]][metric].series[1].data =
-          allSeries[country[1]][metric].series[1].data || [];
-
-        allSeries[country[1]][metric].series[1].data.push(
-          programsSeries.data[index]
+          programsSeries.data[x]
         );
       });
-
-      var selectedSeries = ["vitamin", "programs", "interventions"].map(
-        function(metric) {
-          return (programsSeries = data.series.find(function(series) {
-            return series.name === metric;
-          }));
-        }
-      );
 
       allSeries[country[1]]["splinebar"] =
         allSeries[country[1]]["splinebar"] || {};
@@ -93,7 +68,26 @@ Highcharts.data({
       allSeries[country[1]]["splinebar"].series[2] =
         allSeries[country[1]]["splinebar"].series[2] || {};
 
-      allSeries[country[1]]["splinebar"].series[2].name = "funding";
+      allSeries[country[1]]["splinebar"].series[2].name = "Funding";
+      allSeries[country[1]]["splinebar"].series[2].color = "#4E4154";
+      allSeries[country[1]]["splinebar"].series[2].dataLabels = {
+        enabled: true,
+        format: "${y}M",
+        borderRadius: 2,
+        verticalAlign: "middle",
+        y: -25,
+        shape: "callout",
+        borderColor: "#4E4154",
+        borderWidth: 2,
+        // backgroundColor: "rgba(78, 65, 84, 0.5)",
+        backgroundColor: "rgba(255, 255, 255, 1)",
+        style: {
+          color: "#4E4154",
+          textOutline: "none",
+          // color: "#FFFFFF",
+          fontSize: "14px"
+        }
+      };
 
       allSeries[country[1]]["splinebar"].series[2].yAxis = 0;
 
@@ -101,54 +95,56 @@ Highcharts.data({
         allSeries[country[1]]["splinebar"].series[2].data || [];
 
       allSeries[country[1]]["splinebar"].series[2].data.push(
-        fundingSeries.data[index]
+        fundingSeries.data[x]
       );
 
-      allSeries[country[1]]["splinebar"] =
-        allSeries[country[1]]["splinebar"] || {};
+      var patternArray = [
+        "M 0 0 L 7.5 7.5 M 6.5 -1 L 8.5 1 M -1 6.5 L 1 8.5",
+        "M 0 7.5 L 7.5 0 M -1 1 L 1 -1 M 6.5 8.5 L 8.5 6.5"
+      ];
 
-      allSeries[country[1]]["splinebar"].series[0] =
-        allSeries[country[1]]["splinebar"].series[0] || {};
+      var selectedSeries = ["vitamin", "combined"].map(function(metric, y) {
+        var color = {
+          pattern: {
+            color: "#7DA8D1",
+            path: {
+              d: patternArray[y],
+              strokeWidth: 2
+            },
+            width: 7.5,
+            height: 7.5
+          }
+        };
 
-      allSeries[country[1]]["splinebar"].series[0].name =
-        selectedSeries[0].name;
-      allSeries[country[1]]["splinebar"].series[0].type = "column";
+        allSeries[country[1]]["splinebar"].series[y] =
+          allSeries[country[1]]["splinebar"].series[y] || {};
 
-      allSeries[country[1]]["splinebar"].series[0].yAxis = 1;
+        allSeries[country[1]]["splinebar"].series[y].name = legend[metric];
+        allSeries[country[1]]["splinebar"].series[y].type = "column";
+        allSeries[country[1]]["splinebar"].series[y].color = color;
+        allSeries[country[1]]["splinebar"].series[y].plotOptions = {
+          column: {
+            stacking: "normal"
+          }
+        };
 
-      allSeries[country[1]]["splinebar"].series[0].data =
-        allSeries[country[1]]["splinebar"].series[0].data || [];
+        allSeries[country[1]]["splinebar"].series[y].yAxis = 1;
 
-      allSeries[country[1]]["splinebar"].series[0].data.push(
-        selectedSeries[0].data[index]
-      );
+        allSeries[country[1]]["splinebar"].series[y].data =
+          allSeries[country[1]]["splinebar"].series[y].data || [];
 
-      allSeries[country[1]]["splinebar"] =
-        allSeries[country[1]]["splinebar"] || {};
+        var metricData = data.series.find(function(series) {
+          return series.name === metric;
+        });
 
-      allSeries[country[1]]["splinebar"].series[1] =
-        allSeries[country[1]]["splinebar"].series[1] || {};
-
-      allSeries[country[1]]["splinebar"].series[1].name =
-        selectedSeries[1].name;
-      allSeries[country[1]]["splinebar"].series[1].type = "column";
-
-      allSeries[country[1]]["splinebar"].series[1].yAxis = 1;
-
-      allSeries[country[1]]["splinebar"].series[1].data =
-        allSeries[country[1]]["splinebar"].series[1].data || [];
-
-      selectedSeries[1].data[index]
-        ? allSeries[country[1]]["splinebar"].series[1].data.push(
-            selectedSeries[1].data[index]
-          )
-        : allSeries[country[1]]["splinebar"].series[1].data.push(
-            selectedSeries[2].data[index]
-          );
+        allSeries[country[1]]["splinebar"].series[y].data.push(
+          metricData.data[x]
+        );
+      });
     });
 
-    var container = document.querySelector("#container section");
-    container.dataset.country = "Tanzania";
+    var container = document.querySelector("#container");
+    container.dataset.country = "Tanzania"; ////////////////////////
 
     makeSparkline(
       container.querySelector(".chart"),
@@ -156,31 +152,28 @@ Highcharts.data({
       3
     );
 
-    var sections = Array.from(document.querySelectorAll("section"));
+    var country = container.dataset.country;
 
-    sections.forEach(function(section, sectionIndex) {
-      var country = section.dataset.country;
-      section.querySelector("h2").innerText = country;
+    container.querySelector("h2").innerText = country;
 
-      var statistics = Array.from(
-        section.querySelectorAll(".statistic figure")
-      );
+    var statistics = Array.from(
+      container.querySelectorAll(".statistic figure")
+    );
 
-      statistics.forEach(function(figure, figureIndex) {
-        var metric = figure.dataset.metric;
+    statistics.forEach(function(figure, figureIndex) {
+      var metric = figure.dataset.metric;
 
-        var stat = allSeries[country][metric].series[1].data[1][1];
+      var stat = allSeries[country][metric].series[0].data[1][1];
 
-        if (stat) {
-          figure.innerHTML +=
-            '<h5><span class="stat">' +
-            stat +
-            "%</span><span>" +
-            legend[metric] +
-            report[country] +
-            "</span><h5>";
-        }
-      });
+      if (stat) {
+        figure.innerHTML +=
+          '<h5><span class="stat">' +
+          stat +
+          "%</span><span>" +
+          legend[metric] +
+          report[country] +
+          "</span><h5>";
+      }
     });
   }
 });
@@ -192,51 +185,41 @@ function makeSparkline(figure, series, index) {
     },
     chart: {
       type: "spline",
-      backgroundColor: null,
-      borderWidth: 0,
-      // margin: [0, 0, 0, 0],
-      height: "40%",
-      style: {
-        overflow: "visible"
-      },
-      skipClone: true
+      marginBottom: 100,
+      height: "40%"
     },
-    colors: colorArray,
     xAxis: {
       tickInterval: 1,
       labels: {
         overflow: "allow",
         distance: 0,
-        padding: 0,
-        formatter: function() {
-          return `${this.value.toString().replace("20", "'")}`;
-        }
+        padding: 0
       }
     },
     yAxis: [
       {
-        labels: {
-          formatter: function() {
-            var value = Math.round((this.value / 1000000) * 10) / 10;
-            return `$${value}M`;
-          },
-          style: {
-            color: colorArray[0]
-          }
-        },
         title: {
-          text: "USD",
+          text: "Funding (USD)",
           style: {
-            color: colorArray[0]
+            color: "#4E4154"
           }
         },
-        reversedStacks: true
+        labels: {
+          format: "${value}",
+          style: {
+            color: "#4E4154"
+          }
+        },
+        gridLineColor: "#4E4154",
+        endOnTick: false,
+        reversedStacks: true,
+        opposite: true
       },
       {
         title: {
           text: "Children < 5",
           style: {
-            color: colorArray[index]
+            color: "#7DA8D1"
           }
         },
         labels: {
@@ -245,21 +228,78 @@ function makeSparkline(figure, series, index) {
             return `${value}M`;
           },
           style: {
-            color: colorArray[index]
+            color: "#7DA8D1"
           }
         },
-        reversedStacks: true,
-        opposite: true
+        gridLineColor: "#7DA8D1",
+        endOnTick: false,
+        reversedStacks: true
       }
     ],
-    series: series.map(function(s) {
-      return { ...s, marker: { symbol: "circle" } };
-    }),
+    plotOptions: {
+      column: {
+        groupPadding: 0.25,
+        pointWidth: 100,
+        borderWidth: 2,
+        borderColor: "#7DA8D1"
+      }
+    },
+    series: series,
     legend: {
       enabled: true
     },
     tooltip: {
-      shared: true
+      useHTML: true,
+      headerFormat:
+        "<span style=\"font-size: 18px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: 'Roboto', arial, sans-serif;\">{point.key}</span><br/>",
+      pointFormatter: function() {
+        var index = this.index;
+        var year = this.x;
+        var series = this.series.chart.series;
+
+        var names = [legend["funding"], legend["vitamin"], legend["combined"]];
+
+        var infos = names.map(function(name) {
+          var s = series.find(function(s) {
+            return s.name === name;
+          });
+          return {
+            color: s.color.pattern ? s.color.pattern.color : s.color,
+            y: s.data[index].y,
+            formatter: function(point) {
+              return s.name === "Funding"
+                ? `$${point.y}M`
+                : !point.y
+                  ? `Data Unavailable for ${year}`
+                  : `${Math.round((point.y / 1000000) * 10) /
+                      10} ${name.toLowerCase()}`;
+            }
+          };
+        });
+
+        var textContent = infos
+          .map(function(info) {
+            return `<span style="color:${
+              info.color
+            }">\u25CF </span> <span style="line-height:1.5;font-size: 16px">${info.formatter(info)}</span>`;
+          })
+          .join("<br>");
+
+        return `${textContent}`;
+      }
+    },
+    legend: {
+      title: {
+        text:
+          '<span style="font-size: 12px; color: #808080; font-weight: normal">Click to hide</span>'
+      },
+      itemStyle: {
+        fontSize: "16px",
+        fontWeight: "normal"
+      },
+      useHTML: true,
+      y: 15,
+      align: "bottom"
     },
     credits: {
       text: ""
