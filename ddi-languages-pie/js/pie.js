dc6a72672e2966181acd7b86a4508dc63f25a30b
leezaweb <leeza.web@gmail.com>
2019-04-24 10:55:34 -0400

    start pie

---
+++ ddi-languages-pie/js/pie.js
@@ -0,0 +1,145 @@
+Highcharts.setOptions({
+  lang: {
+    thousandsSep: ","
+  },
+
+  colors: [
+    "#344a45",
+    "#3a5a56",
+    "#3e6a69",
+    "#437a7f",
+    "#488b96",
+    "#4f9cae",
+    "#59acc8",
+    "#67bce2",
+    "#0095AB"
+  ]
+});
+
+Highcharts.chart("container", {
+  chart: {
+    type: "pie",
+    margin: [10, 0, 50, 0],
+    styledMode: true
+  },
+  title: {
+    text: ""
+  },
+  subtitle: {
+    text: ""
+  },
+  credits: {
+    href: true,
+    text:
+      'CSIS| Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+  },
+  plotOptions: {
+    pie: {
+      size: "100%",
+      states: {
+        inactive: {
+          opacity: 0.75
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        format: "{point.name}<br> ({point.y:,f} tweets)"
+      }
+    }
+  },
+  drilldown: {
+    drillUpButton: {
+      position: { align: "left", y: -50 },
+      relativeTo: "spacingBox"
+    }
+  },
+  tooltip: {
+    headerFormat:
+      '<span style="color:{point.color}"> \u25CF</span> <span style="font-size: 13px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: \'Roboto\', arial, sans-serif;"> {point.key}</span><br/>',
+
+    pointFormatter: function() {
+      var suffix = "%</b> of total<br/>";
+
+      return this.series.options._levelNumber
+        ? (this.percentage / 10).toFixed(1) + suffix
+        : this.percentage.toFixed(1) + suffix;
+    }
+  },
+
+  series: [
+    {
+      name: "Languages",
+      colorByPoint: true,
+      data: [
+        {
+          name: "German",
+          y: 99332,
+          drilldown: null
+        },
+        {
+          name: "Ukrainian",
+          y: 82237,
+          drilldown: null
+        },
+        {
+          name: "Bulgarian",
+          y: 54690,
+          drilldown: null
+        },
+        {
+          name: "Italian",
+          y: 20376,
+          drilldown: null
+        },
+        {
+          name: "Spanish",
+          y: 12802,
+          drilldown: null
+        },
+        {
+          name: "French",
+          y: 12636,
+          drilldown: null
+        },
+        {
+          name: "Serbian",
+          y: 8045,
+          drilldown: null
+        },
+        {
+          name: "Estonian",
+          y: 6146,
+          drilldown: null
+        },
+        { name: "Lesser Targets", y: 32826, drilldown: "Lesser Targets" }
+      ]
+    }
+  ],
+  drilldown: {
+    series: [
+      {
+        name: "Lesser Targets",
+        id: "Lesser Targets",
+        data: [
+          ["Turkish", 3927],
+          ["Dutch", 3541],
+          ["Danish", 3483],
+          ["Slovakian", 3343],
+          ["Romanian", 3161],
+          ["Portugese", 2904],
+          ["Welsh", 2677],
+          ["Slovenian", 2347],
+          ["Polish", 1921],
+          ["Swedish", 1863],
+          ["Finnish", 1794],
+          ["Norwegian", 1605],
+          ["Lithuanian", 1453],
+          ["Czech", 1070],
+          ["Bosnian", 615],
+          ["Latvian", 525],
+          ["Hungarian", 519]
+        ]
+      }
+    ]
+  }
+});
