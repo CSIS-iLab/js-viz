598a87ff6c9d68508b6b94fabd1bb79dc17c6dac
leezaweb <leeza.web@gmail.com>
2019-07-01 13:39:27 -0400

    two options

--- iframe-example/script.js
+++ iframe-example/script.js
@@ -36,7 +36,7 @@ gapi.load('client', function() {
             rows: sheet.result.values.slice(4)
           }
 
-          Highcharts.chart('container', {
+          Highcharts.chart('container1', {
             chart: {
               type: 'column'
               // type: 'area'
@@ -81,6 +81,106 @@ gapi.load('client', function() {
                   s.name = i === 8 ? s.name : 'PB ' + s.name
                   s.color = i === 8 ? 'black' : chartColors[i]
                   s.type = i === 8 ? 'line' : 'area'
+                })
+
+                d.series = newSeries
+              }
+            },
+
+            credits: {
+              href: false,
+              position: { align: 'center' },
+              text: sheetData.credits
+            },
+            xAxis: {
+              allowDecimals: false
+            },
+            yAxis: {
+              allowDecimals: false,
+              title: {
+                text: sheetData.yAxis,
+                margin: 10,
+                x: -15
+              }
+            },
+            tooltip: {
+              shared: true
+            },
+            legend: {
+              align: 'center',
+              verticalAlign: 'bottom',
+              layout: 'horizontal',
+              itemStyle: {
+                textOverflow: null
+              }
+            },
+            plotOptions: {
+              area: {
+                marker: {
+                  enabled: false,
+                  symbol: 'circle'
+                }
+              },
+              column: {
+                stacking: 'normal',
+                maxPointWidth: 150
+              },
+
+              line: {
+                dashStyle: 'LongDash',
+                marker: {
+                  enabled: false,
+                  symbol: 'circle'
+                }
+              }
+            }
+          })
+
+          Highcharts.chart('container2', {
+            chart: {
+              type: 'column'
+              // type: 'area'
+              // type: 'line'
+            },
+            title: {
+              text: sheetData.title
+            },
+            data: {
+              switchRowsAndColumns: true,
+              csv: sheetData.rows.map(r => r.join(',')).join('\n'),
+              complete: d => {
+                let newSeries = d.series
+                  .filter(s => s.name.toLowerCase().indexOf('actual') < 0)
+                  .reduce(function(total, obj) {
+                    let name = obj.name.split(' ')[1]
+
+                    let series = total.find(s => s.name === name)
+
+                    if (!series) {
+                      total.push({
+                        name: name ? name : 'Actual',
+                        data: obj.data.map(d => [d[0], d[1] ? 0 : undefined])
+                      })
+                      series = total.find(s => s.name === name)
+                    }
+
+                    series.data = series.data.map((d, i) => [
+                      d[0],
+                      (d[1] += obj.data[i][1])
+                    ])
+
+                    return total
+                  }, [])
+
+                let actualSeries = d.series.find(
+                  s => s.name.toLowerCase().indexOf('actual') > -1
+                )
+
+                newSeries.push({ ...actualSeries })
+                newSeries.forEach((s, i) => {
+                  s.name = i === 8 ? s.name : 'PB ' + s.name
+                  s.color = i === 8 ? 'black' : chartColors[i]
+                  s.type = i === 8 ? 'line' : 'line'
                 })
 
                 d.series = newSeries
