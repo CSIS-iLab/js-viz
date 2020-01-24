f7fc49b5bcdc74bd25088ba48fdb1a79c575ff93
Lindsay Urchyk <urchykli@gmail.com>
2020-01-24 16:20:11 -0500

    Diversity page bar charts

---
+++ diversity-page/gender/js/bar.js
@@ -0,0 +1,55 @@
+Highcharts.chart('hcContainer', {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: '1susmb2O7l-sRkILnlxZyatXmtebTrGjQk_K-qPqjzgs',
+    googleSpreadsheetWorksheet: 1
+  },
+  // General Chart Options
+  chart: {
+    type: 'column'
+  },
+  // Chart Title and Subtitle
+  title: {
+    text: "CSIS Staff Breakdown by Gender"
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS"
+  },
+  // Chart Legend
+  legend: {
+    title: {
+      text: '<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
+    },
+    align: 'center',
+    verticalAlign: 'bottom',
+    layout: 'horizontal'
+  },
+  //  Tooltip 
+  tooltip: {
+    shared: true,
+  },
+  // X Axis
+  xAxis: {
+    type: 'category',
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "Number of Employees"
+    },
+  },
+  // Additional Plot Options
+  plotOptions:
+  {
+    column: {
+      // stacking: null, // Normal bar graph
+      stacking: "normal", // Stacked bar graph
+      dataLabels: {
+        enabled: false,
+      }
+    }
+  }
+})
