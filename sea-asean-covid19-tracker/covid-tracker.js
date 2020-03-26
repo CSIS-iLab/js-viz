1861c2dc56b2ad70283b37a376cf621e56c2df36
mariel-delagarza <mariel.code@gmail.com>
2020-03-26 16:57:50 -0400

    Create js file and move js code inside it

---
+++ sea-asean-covid19-tracker/covid-tracker.js
@@ -0,0 +1,44 @@
+/*Use Tabletop and Datatables to read Google Spreadsheet*/
+
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1FaJQvGn-DjUNyFPyAkurQrBlVULRJ2GHFDEJ0cjolHw/edit?usp=sharing';
+      
+/*init() and showInfo() are from Tabletop, with the addition of displayInfo to use Datatables*/
+function init() {
+  Tabletop.init( { 
+    key: publicSpreadsheetUrl,
+    callback: showInfo,
+    parseNumbers: true,
+    simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+  console.log(data)
+  console.log(tabletop.modelNames[0])
+  let lastUpdatedEl = document.getElementById("lastUpdated")
+  lastUpdatedEl.innerHTML = tabletop.modelNames[0]
+  displayInfo(data)
+}
+
+      
+/*function to use Datatables to display data */
+function displayInfo(dataset) {
+  $('#example').DataTable( {
+    responsive: true,
+    data: dataset,
+    columns: [
+      { title: 'Country', data: 'Country' },
+      { title: 'Total Cases', data: 'Total Cases', className:'dt-body-right' },
+      { title: 'Cases Last 24hr', data: 'Cases Last 24hr', className: 'dt-body-right' },
+      { title: 'Total Deaths', data: 'Total Deaths', className: 'dt-body-right' },
+      { title: '# Tests', data: '# Tests', className: 'dt-body-right' },
+      { title: '# Recovered', data: '# Recovered', className: 'dt-body-right' },
+      { title: 'Cases per Million', data: 'Cases per Million', className: 'dt-body-right' },
+      { title: 'Total Population', data: 'Total Population', className: 'dt-body-right' }
+    ],
+    paging: false,
+    searching: false,
+    info: false
+  });
+} 
+
+window.addEventListener('DOMContentLoaded', init)
