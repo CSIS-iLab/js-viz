f25650ef8b5450599a7ff0bb4ba60978a45799fb
mariel-delagarza <mariel.code@gmail.com>
2020-03-27 10:57:08 -0400

    Update column titles, remove # and total from display

--- sea-asean-covid19-tracker/covid-tracker.js
+++ sea-asean-covid19-tracker/covid-tracker.js
@@ -25,13 +25,13 @@ function displayInfo(dataset) {
     data: dataset,
     columns: [
       { title: 'Country', data: 'Country' },
-      { title: 'Total Cases', data: 'Total Cases', className:'dt-body-right' },
+      { title: 'Cases', data: 'Total Cases', className:'dt-body-right' },
       { title: 'Cases Last 24hr', data: 'Cases Last 24hr', className: 'dt-body-right' },
-      { title: 'Total Deaths', data: 'Total Deaths', className: 'dt-body-right' },
-      { title: '# Tests', data: '# Tests', className: 'dt-body-right' },
-      { title: '# Recovered', data: '# Recovered', className: 'dt-body-right' },
+      { title: 'Deaths', data: 'Total Deaths', className: 'dt-body-right' },
+      { title: 'Tests', data: '# Tests', className: 'dt-body-right' },
+      { title: 'Recovered', data: '# Recovered', className: 'dt-body-right' },
       { title: 'Cases per Million', data: 'Cases per Million', className: 'dt-body-right' },
-      { title: 'Total Population', data: 'Total Population', className: 'dt-body-right' }
+      { title: 'Population', data: 'Total Population', className: 'dt-body-right' }
     ],
     paging: false,
     searching: false,
