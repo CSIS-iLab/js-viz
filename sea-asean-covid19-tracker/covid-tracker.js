569ddf466e25686737ccd8f1c51f01d203105c01
Jacque Schrag <jnschrag@gmail.com>
2020-03-27 11:18:39 -0400

    Design tweaks to table

--- sea-asean-covid19-tracker/covid-tracker.js
+++ sea-asean-covid19-tracker/covid-tracker.js
@@ -1,42 +1,55 @@
 /*Use Tabletop and Datatables to read Google Spreadsheet*/
 
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1FaJQvGn-DjUNyFPyAkurQrBlVULRJ2GHFDEJ0cjolHw/edit?usp=sharing';
-      
+var publicSpreadsheetUrl =
+  "https://docs.google.com/spreadsheets/d/1FaJQvGn-DjUNyFPyAkurQrBlVULRJ2GHFDEJ0cjolHw/edit?usp=sharing";
+
 /*init() and showInfo() are from Tabletop, with the addition of displayInfo to use Datatables*/
 function init() {
-  Tabletop.init( { 
+  Tabletop.init({
     key: publicSpreadsheetUrl,
     callback: showInfo,
     parseNumbers: true,
-    simpleSheet: true } )
+    simpleSheet: true
+  });
 }
 
 function showInfo(data, tabletop) {
-  let lastUpdatedEl = document.getElementById("lastUpdated")
-  lastUpdatedEl.innerHTML = "<em>Last Updated: </em>" +  tabletop.modelNames[0] + "<br/><em>Source</em>: "
-  displayInfo(data)
+  let lastUpdatedEl = document.getElementById("lastUpdated");
+  lastUpdatedEl.innerHTML = tabletop.modelNames[0];
+  displayInfo(data);
 }
 
-      
 /*function to use Datatables to display data */
 function displayInfo(dataset) {
-  $('#example').DataTable( {
+  $("#table").DataTable({
     responsive: true,
     data: dataset,
     columns: [
-      { title: 'Country', data: 'Country' },
-      { title: 'Cases', data: 'Total Cases', className:'dt-body-right' },
-      { title: 'Cases Last 24hr', data: 'Cases Last 24hr', className: 'dt-body-right' },
-      { title: 'Deaths', data: 'Total Deaths', className: 'dt-body-right' },
-      { title: 'Tests', data: '# Tests', className: 'dt-body-right' },
-      { title: 'Recovered', data: '# Recovered', className: 'dt-body-right' },
-      { title: 'Cases per Million', data: 'Cases per Million', className: 'dt-body-right' },
-      { title: 'Population', data: 'Total Population', className: 'dt-body-right' }
+      { title: "Country", data: "Country" },
+      { title: "Cases", data: "Total Cases", className: "dt-body-right" },
+      {
+        title: "Cases Last 24hr",
+        data: "Cases Last 24hr",
+        className: "dt-body-right"
+      },
+      { title: "Deaths", data: "Total Deaths", className: "dt-body-right" },
+      { title: "Tests", data: "# Tests", className: "dt-body-right" },
+      { title: "Recovered", data: "# Recovered", className: "dt-body-right" },
+      {
+        title: "Cases per Million",
+        data: "Cases per Million",
+        className: "dt-body-right"
+      },
+      {
+        title: "Population",
+        data: "Total Population",
+        className: "dt-body-right"
+      }
     ],
     paging: false,
     searching: false,
     info: false
   });
-} 
+}
 
-window.addEventListener('DOMContentLoaded', init)
+window.addEventListener("DOMContentLoaded", init);
