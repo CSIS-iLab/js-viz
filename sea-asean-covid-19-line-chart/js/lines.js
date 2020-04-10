056505b48643b242bc02d831c391b42453f9e239
mariel-delagarza <mariel.code@gmail.com>
2020-04-10 16:19:04 -0400

    Change chart title

--- sea-asean-covid-19-line-chart/js/lines.js
+++ sea-asean-covid-19-line-chart/js/lines.js
@@ -11,7 +11,7 @@ Highcharts.chart('hcContainer', {
   },
   // Chart Title and Subtitle
   title: {
-    text: "Interactive Title"
+    text: "Chart Tracker COVID19 SEA"
   },
   subtitle: {
     text: "Click and drag to zoom in"
