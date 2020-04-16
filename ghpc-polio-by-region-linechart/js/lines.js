b90f33bd844170fd42e52530678f8b2287827ddb
mariel-delagarza <mariel.code@gmail.com>
2020-04-16 14:35:50 -0400

    Copy over js file from past cpower chart/template

---
+++ ghpc-polio-by-region-linechart/js/lines.js
@@ -0,0 +1,64 @@
+Highcharts.chart("hcContainer", {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: "1s9bdw5ob4BpCEUUQl-4lQnTQSsi7S-q927lKT7k6l6k",
+    googleSpreadsheetWorksheet: 2,
+    parseDate: function(thisDate) {
+      return Date.parse(thisDate.replace('-', '/1/'));
+    }
+  },
+  // General Chart Options
+  chart: {
+    type: "line"
+  },
+  // Colors
+  colors: [
+    '#58a897',
+    '#3b75bb',
+    '#a483a8',
+    '#8cb561',
+    '#ef9a9a',
+    '#ed392a'
+  ],
+  // Chart Title and Subtitle
+  title: {
+    text: "Breakdown of International Payments by Currency"
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS China Power Project | Source: SWIFT"
+  },
+  // Chart Legend
+  legend: {
+    align: "center",
+    verticalAlign: "bottom",
+    layout: "horizontal"
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "Share of Total Payments (%)"
+    }
+  },
+  // Tooltip
+  tooltip: {
+    valueDecimals: 1,
+    shared: true,
+    useHTML: true,
+    valueSuffix: '%',
+    xDateFormat: '%B %Y'
+  },
+  // Additional Plot Options
+  plotOptions: {
+    line: {
+      marker: {
+        enabled: false,
+        symbol: "circle",
+        radius: 3
+      },
+      lineWidth: 3
+    }
+  }
+});
