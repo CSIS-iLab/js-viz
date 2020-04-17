ee52f18401c62d91ecc0fcab2d1970fb1f8a318b
mariel-delagarza <mariel.code@gmail.com>
2020-04-17 16:07:00 -0400

    Add chart subtitle and styling

--- ghpc-polio-by-region-linechart/js/spline.js
+++ ghpc-polio-by-region-linechart/js/spline.js
@@ -39,6 +39,9 @@ Highcharts.chart("hcContainer", {
   title: {
     text: '<span style="font-size: 32px; color: #333333; max-width: 1280px; text-align: center">Global Polio Cases by Region, 1980-2019</span>'
   },
+  subtitle: {
+    text: '<span style="color: #333333; max-width: 1280px; margin-bottom: 1.5rem; line-height: 1.4">Hover over a line to see the number of cases per region in that year. Click and drag to zoom.</span>'
+  },
   // Credits
   credits: {
     enabled: true,
