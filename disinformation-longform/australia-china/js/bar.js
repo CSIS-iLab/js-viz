a56906c9cad652e00e762d8264991e3482b3c425
mariel-delagarza <mariel.code@gmail.com>
2020-06-23 14:03:38 -0400

    First chart drafts

--- disinformation-longform/australia-china/js/bar.js
+++ disinformation-longform/australia-china/js/bar.js
@@ -1,96 +1,66 @@
 // Data gathered from http://populationpyramid.net/germany/2015/
 
 // Age categories
-var categories = [
-  '0-4', '5-9', '10-14', '15-19',
-  '20-24', '25-29', '30-34', '35-39', '40-44',
-  '45-49', '50-54', '55-59', '60-64', '65-69',
-  '70-74', '75-79', '80-84', '85-89', '90-94',
-  '95-99', '100 + '
-];
+var categories
 
-Highcharts.chart('hcContainer', {
-  chart: {
-      type: 'bar'
-  },
-  title: {
-      text: "Australia's Ratings of China"
-  },
-  subtitle: {
-      text: ''
-  },
-  credits: {
-    enabled: true,
-    href: false,
-    text: "CSIS [Project] | Source: PEW"
-  },
-  xAxis: [{
-      categories: categories,
-      reversed: false,
-      labels: {
-          step: 1
-      },
-      accessibility: {
-          description: 'Age (male)'
-      }
-  }, { // mirror axis on right side
-      opposite: true,
-      reversed: false,
-      categories: categories,
-      linkedTo: 0,
-      labels: {
-          step: 1
-      },
-      accessibility: {
-          description: 'Age (female)'
-      }
-  }],
-  yAxis: {
-      title: {
-          text: null
-      },
-      labels: {
-          formatter: function () {
-              return Math.abs(this.value) + '%';
-          }
-      },
-      accessibility: {
-          description: 'Percentage population',
-          rangeDescription: 'Range: 0 to 5%'
-      }
-  },
+Highcharts.chart("hcContainer", {
+    data: {
+        googleSpreadsheetKey: "1uf6yWzyUSU_-oQ85lK3695iQbWL_YbcmWB00PnZvxlA",
+        googleSpreadsheetWorksheet: 3
+    },
+    chart: {
+        type: "bar",
+    },
+    title: {
+        text: "Australia's Ratings of China",
+    },
+    subtitle: {
+        text: "",
+    },
+    credits: {
+        enabled: true,
+        href: false,
+        text: "CSIS | Source: PEW",
+    },
+    xAxis: [{
+            categories: categories,
+            reversed: true,
+            tickInterval: 1,
+        },
+        {
+            // mirror axis on right side
+            opposite: true,
+            categories: categories,
+            linkedTo: 0
+        },
+    ],
+    yAxis: {
+        title: "",
+        min: -100,
+        max: 100,
+        labels: {
+            formatter: function () {
+                return Math.abs(this.value) + "%";
+            },
+        }
+    },
 
-  plotOptions: {
-      series: {
-          stacking: 'normal'
-      }
-  },
+    plotOptions: {
+        series: {
+            stacking: "normal",
+        },
+    },
 
-  tooltip: {
-      formatter: function () {
-          return '<b>' + this.series.name + ', age ' + this.point.category + '</b><br/>' +
-              'Population: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1) + '%';
-      }
-  },
-
-  series: [{
-      name: 'Male',
-      data: [
-          -2.2, -2.1, -2.2, -2.4,
-          -2.7, -3.0, -3.3, -3.2,
-          -2.9, -3.5, -4.4, -4.1,
-          -3.4, -2.7, -2.3, -2.2,
-          -1.6, -0.6, -0.3, -0.0,
-          -0.0
-      ]
-  }, {
-      name: 'Female',
-      data: [
-          2.1, 2.0, 2.1, 2.3, 2.6,
-          2.9, 3.2, 3.1, 2.9, 3.4,
-          4.3, 4.0, 3.5, 2.9, 2.5,
-          2.7, 2.2, 1.1, 0.6, 0.2,
-          0.0
-      ]
-  }]
+    tooltip: {
+        formatter: function () {
+            return (
+                "<b>" +
+                this.series.name + ", " +
+                this.point.category +
+                "</b><br/>" +
+                Highcharts.numberFormat(Math.abs(this.point.y), 1) +
+                "%"
+            );
+        },
+    },
 });
