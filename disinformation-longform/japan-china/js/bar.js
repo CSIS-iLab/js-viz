22eaab6e919ca4060a155c90377a32d9cbf4b9c5
Mariel <mariel.code@gmail.com>
2020-06-29 16:54:47 -0400

    Disinformation longform (#53)
    * Creation of project files
    
    * First chart drafts
    
    * Remove tickInterval setting
    
    * Update tooltip formatting, chart colors
    
    * Swap order of sentences in subtitle
    
    * Change font from default to Roboto
    
    * Change title size, set tickInterval to 2
    
    * First draft of Russia/China comparison charts
    
    * Remove data before 2013
    
    * Update tooltip position
    
    * Update country colors, update font to Roboto
    
    * Adjust subtitle
    
    * Update chart titles as example
    
    * Update title
    
    * Update titles for 1:1 graphs
    
    * Update title
    
    * Update subtitle
    
    * Update title
    
    * Update tooltip location
    
    * Make requested changes
    
    * Update tooltip - requested chanegs
    
    * Remove empty/unused categories variable
    
    * Make requested changes - y-axis title to null
    
    * Make requested changes
    
    * Update tooltip position

---
+++ disinformation-longform/japan-china/js/bar.js
@@ -0,0 +1,84 @@
+Highcharts.chart('hcContainer', {
+    data: {
+        googleSpreadsheetKey: "1uf6yWzyUSU_-oQ85lK3695iQbWL_YbcmWB00PnZvxlA",
+        googleSpreadsheetWorksheet: 4
+    },
+    chart: {
+        type: 'bar',
+        style: {
+            fontFamily: 'Roboto'
+        }
+    },
+    title: {
+        text: "Japan: Opinions on China",
+        style: {
+            fontSize: '24px'
+        }    
+    },
+    subtitle: {
+        text: 'Hover to see the percentage of favorable or unfavorable responses toward China.'
+    },
+    credits: {
+        enabled: true,
+        href: false,
+        text: "CSIS | Source: PEW"
+    },
+    xAxis: [{
+            reversed: true,
+            tickInterval: 2
+        },
+        {
+            // mirror axis on right side
+            opposite: true,
+            linkedTo: 0
+        },
+    ],
+    yAxis: {
+        title: null,
+        max: 100,
+        min: -100,
+        labels: {
+            formatter: function () {
+                return Math.abs(this.value) + "%";
+            },
+        }
+    },
+    plotOptions: {
+        series: {
+            stacking: "normal",
+        },
+    },
+    tooltip: {
+        shared: false,
+        borderColor: 'gray',
+        headerFormat: '<span style="font-size: 14px">{point.key}</span><br/>',
+        pointFormatter: function () {
+            var result = this.y
+                if (result < 0) {
+                    result = result * -1
+                } else if (result > 0) {
+                    result 
+                }
+            
+            return  '<span style="font-size: 14px;color:' + this.color + '">\u25CF</span> ' + this.series.name + ': <b>' + result + '%</b><br/>'
+        }
+        
+    },
+    series: [
+      {
+        name: "Favorable",
+        color: "#a43135",
+        legendIndex: 2,
+        states: {
+            inactive: {opacity: 1}
+        }
+      },
+      { name: "Unfavorable",
+        color: "#7cb5ec",
+        legendIndex: 1,
+        states: {
+            inactive: {opacity: 1}
+        }
+      }
+    ]
+});
---
+++ disinformation-longform/russia-china-unfavorable/js/column.js
@@ -0,0 +1,72 @@
+Highcharts.chart('hcContainer', {
+  data: {
+    googleSpreadsheetKey: "1uf6yWzyUSU_-oQ85lK3695iQbWL_YbcmWB00PnZvxlA",
+    googleSpreadsheetWorksheet: 8
+  },
+  chart: {
+      type: 'column',
+      style: {
+        fontFamily: 'Roboto'
+    }
+  },
+  title: {
+      text: "Case Study Countries' Unfavorable Opinions on Russia and China",
+      style: {
+        fontSize: '24px'
+    }   
+  },
+  subtitle: {
+    text: "Russia's scores are the average of polling conducted in that year in the United Kingdom and Germany. <br>China's scores are the average of polling conducted in that year in Australia and Japan.<br> Years where one country was missing data are omitted. <br>Hover to see the overall percentage of unfavorable responses to Russia and China."
+  },
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS | Source: PEW",
+  } ,
+  yAxis: {
+    title: "",
+    min: 0,
+    max: 100,
+    labels: {
+        formatter: function () {
+            return Math.abs(this.value) + "%";
+        },
+    }
+  },
+  plotOptions: {
+      column: {
+          stacking: ''
+      },
+    series: {
+      centerInCategory: true
+    }
+  },
+  tooltip: {
+    shared: true,
+    positioner: function(w, h, p) {
+      return {
+          x: p.plotX + this.chart.hoverSeries.xAxis.left - w/2,
+          y: p.plotY + 150
+      }
+    },
+    borderColor: 'gray',
+    headerFormat: '<span style="font-size: 14px">{point.key}</span><br/>',
+    valueDecimals: 2,
+    valueSuffix: "%"
+  },
+  series: [
+    {
+      name: "Russia",
+      color: "#000",
+      states: {
+          inactive: {opacity: 1}
+      }
+    },
+    { name: "China",
+      color: "#a43135",
+      states: {
+          inactive: {opacity: 1}
+      }
+    }
+  ]
+});
