3fe7625fb9d8ad133a134981d0c688c150139b97
Lindsay Urchyk <urchykli@gmail.com>
2020-07-14 18:12:16 -0400

    ISP D&I page charts (#63)
    * ISP Diversity Page initial commit
    
    * ISP Diversity & Inclusion bar charts

---
+++ isp-diversity-page/ethnicity/js/bar.js
@@ -0,0 +1,58 @@
+Highcharts.chart("hcContainer", {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: "16poMVWLNa7B8oTs6zcM1pj4Pmr6Smbmwxt7dg4Xwz-o",
+    googleSpreadsheetWorksheet: 2,
+  },
+  // General Chart Options
+  chart: {
+    type: "column",
+  },
+  // Colors
+  colors: ["#66C6CB", "#0092A8", "#013446"],
+  // Chart Title and Subtitle
+  title: {
+    text: "Race/Ethnicity*, July 2020",
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS | Center for Strategic and International Studies",
+  },
+  // Chart Legend
+  legend: {
+    title: {
+      text:
+        '<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>',
+    },
+    align: "center",
+    verticalAlign: "bottom",
+    layout: "horizontal",
+    reversed: true,
+  },
+  //  Tooltip
+  tooltip: {
+    shared: true,
+  },
+  // X Axis
+  xAxis: {
+    type: "category",
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "Number of Employees",
+    },
+  },
+  // Additional Plot Options
+  plotOptions: {
+    column: {
+      // stacking: null, // Normal bar graph
+      stacking: "normal", // Stacked bar graph
+      dataLabels: {
+        enabled: false,
+      },
+    },
+  },
+});
---
+++ isp-diversity-page/veteran/js/bar.js
@@ -0,0 +1,57 @@
+Highcharts.chart("hcContainer", {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: "16poMVWLNa7B8oTs6zcM1pj4Pmr6Smbmwxt7dg4Xwz-o",
+    googleSpreadsheetWorksheet: 1,
+  },
+  // General Chart Options
+  chart: {
+    type: "column",
+  },
+  // Colors
+  colors: ["#66C6CB", "#0092A8", "#013446"],
+  // Chart Title and Subtitle
+  title: {
+    text: "Veteran Status, July 2020",
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS | Center for Strategic and International Studies",
+  },
+  // Chart Legend
+  legend: {
+    title: {
+      text:
+        '<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>',
+    },
+    align: "center",
+    verticalAlign: "bottom",
+    layout: "horizontal",
+  },
+  //  Tooltip
+  tooltip: {
+    shared: true,
+  },
+  // X Axis
+  xAxis: {
+    type: "category",
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "Number of Employees",
+    },
+  },
+  // Additional Plot Options
+  plotOptions: {
+    column: {
+      // stacking: null, // Normal bar graph
+      stacking: "normal", // Stacked bar graph
+      dataLabels: {
+        enabled: false,
+      },
+    },
+  },
+});
