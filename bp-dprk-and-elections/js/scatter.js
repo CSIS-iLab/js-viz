1ca3bd48ebbff587a760ec9b025a7f66039e9b69
Lindsay Urchyk <urchykli@gmail.com>
2020-07-17 16:06:18 -0400

    Call render chart function

--- bp-dprk-and-elections/js/scatter.js
+++ bp-dprk-and-elections/js/scatter.js
@@ -1,18 +1,18 @@
 //regionData
-let yearData = []
+let yearData = [];
 
 // regionArray
-let yearArray = []
+let yearArray = [];
 // used to calculate yearArray
-let allYears = []
+let allYears = [];
 
-let electionYearAndDays = {}
-let mins = []
-let maxes = []
+let electionYearAndDays = {};
+let mins = [];
+let maxes = [];
 
 Highcharts.data({
   // Load Data in from Google Sheets
-  googleSpreadsheetKey: '11nBD55d0t4QE1h-OeymUg897azDIC6CHME2UenQ8WSw',
+  googleSpreadsheetKey: "11nBD55d0t4QE1h-OeymUg897azDIC6CHME2UenQ8WSw",
   googleSpreadsheetWorksheet: 2,
   switchRowsAndColumns: true,
   parsed: function parsed(columns) {
@@ -28,30 +28,28 @@ Highcharts.data({
 
       if (electionYear >= "1990") {
         if (!yearArray.includes(electionYear)) {
-          yearArray.push(electionYear)
+          yearArray.push(electionYear);
         }
         if (!electionYearAndDays[electionYear]) {
-          electionYearAndDays[electionYear] = []
+          electionYearAndDays[electionYear] = [];
         }
-        electionYearAndDays[electionYear].push(numberOfDays)
+        electionYearAndDays[electionYear].push(numberOfDays);
       }
     });
 
     yearArray.forEach((year) => {
-      let min = Math.min(...electionYearAndDays[year])
+      let min = Math.min(...electionYearAndDays[year]);
       if (!mins.includes(min)) {
-        mins.push(min)
+        mins.push(min);
       }
-    })
-    console.log(mins)
+    });
 
     yearArray.forEach((year) => {
-      let max = Math.max(...electionYearAndDays[year])
+      let max = Math.max(...electionYearAndDays[year]);
       if (!maxes.includes(max)) {
-        maxes.push(max)
+        maxes.push(max);
       }
-    })
-    console.log(maxes)
+    });
 
     for (i = 0; i < yearArray.length; i++) {
       yearData.push({
@@ -59,36 +57,37 @@ Highcharts.data({
         x2: maxes[i],
         y: i,
         year: yearArray[i],
-        color: 'lightGray',
-        points: electionYearAndDays[yearArray[i]]
-      })
+        color: "lightGray",
+        // points: electionYearAndDays[yearArray[i]],
+      });
     }
-  }
-})
+    renderChart(yearData, yearArray);
+  },
+});
 
 function renderChart(yearData, yearArray) {
   Highcharts.setOptions({
     lang: {
-      thousandsSep: ","
-    }
-  })
-
-  Highcharts.chart('hcContainer', {
+      thousandsSep: ",",
+    },
+  });
+  Highcharts.chart("hcContainer", {
     chart: {
-      type: 'scatter',
-      height: '80%'
+      // type: "xrange",
+      height: "80%",
     },
     exporting: {
       sourceWidth: 723,
       sourceHeight: 775,
       chartOptions: {
         title: {
-          text: "The Triple Burden of Malnutrition in Tanzania"
+          text: "The Triple Burden of Malnutrition in Tanzania",
         },
         subtitle: {
-          text: "This chart shows the co-occurrence of three major types of malnutrition—overweight or obese, stunting, and anemia. The key population for stunting, or below average height for age, is children under 5. Women of reproductive age (15 to 49 years) are the key population for anemia and overweight or obese."
-        }
-      }
+          text:
+            "This chart shows the co-occurrence of three major types of malnutrition—overweight or obese, stunting, and anemia. The key population for stunting, or below average height for age, is children under 5. Women of reproductive age (15 to 49 years) are the key population for anemia and overweight or obese.",
+        },
+      },
     },
     // Chart Title and Subtitle
     title: {
@@ -103,27 +102,27 @@ function renderChart(yearData, yearArray) {
       href: false,
       text: "CSIS Beyond Parallel | Source: ",
       position: {
-        y: 0
-      }
+        y: 0,
+      },
     },
     // Chart Legend
     legend: {
-      align: 'left',
-      verticalAlign: 'top',
-      useHTML: true
-      },
-      // remove default formatting
-      symbolHeight: 0,
-      symbolWidth: 0,
-      symbolRadius: 0,
-      itemStyle: {
-        cursor: 'default'
+      align: "left",
+      verticalAlign: "top",
+      useHTML: true,
+    },
+    // remove default formatting
+    symbolHeight: 0,
+    symbolWidth: 0,
+    symbolRadius: 0,
+    itemStyle: {
+      cursor: "default",
     },
     // X Axis
     xAxis: {
       title: {
         enabled: true,
-        text: 'Days before or after an Election',
+        text: "Days before or after an Election",
       },
       maxPadding: 0.15, // extend axis to 60%
       minPadding: 0.3, // extend axis to 0%
@@ -134,49 +133,52 @@ function renderChart(yearData, yearArray) {
     // Y Axis
     yAxis: {
       title: {
-        enabled: 'Election Year'
+        enabled: true,
+        text: "Election Year",
       },
       categories: yearArray,
-      gridLineColor: 'transparent',
+      gridLineColor: "transparent",
     },
     // Additional Plot Options
-    plotOptions:
-    {
+    plotOptions: {
       series: {
         borderWidth: 0,
         pointPadding: 0,
         groupPadding: 0,
         events: {
           legendItemClick: function () {
-            return false
-          }
-        }
+            return false;
+          },
+        },
       },
       xrange: {
-        enableMouseTracking: false
+        enableMouseTracking: false,
       },
       scatter: {
-        showInLegend: true
-      }
+        showInLegend: true,
+      },
     },
     tooltip: {
       useHTML: true,
-      backgroundColor: 'rgb(255, 255, 255)'
+      backgroundColor: "rgb(255, 255, 255)",
     },
-    series: [{
-      type: 'xrange',
-      pointWidth: 2,
-      id: 'main',
-      name: "Election Year",
-      data: yearData,
-      showInLegend: false
-    }, {
-      type: 'scatter',
-      linkedTo: 'main',
-      marker: {
-        radius: 3.5
+    series: [
+      {
+        type: "xrange",
+        pointWidth: 2,
+        id: "main",
+        name: "Election Year",
+        data: yearData,
+        showInLegend: false,
       },
-      data: dataPoints
-    }]
-  })
+      // {
+      //   type: "scatter",
+      //   linkedTo: "main",
+      //   marker: {
+      //     radius: 3.5,
+      //   },
+      //   data: newArray,
+      // },
+    ],
+  });
 }
