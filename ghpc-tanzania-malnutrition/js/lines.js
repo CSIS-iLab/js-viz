8d72a90c6642eed45ad2152bd455da780fdb3424
mariel-delagarza <mariel.code@gmail.com>
2020-07-31 14:57:45 -0400

    Copy js file for tanzania chart over from master - remove changes from running prettier

--- ghpc-tanzania-malnutrition/js/lines.js
+++ ghpc-tanzania-malnutrition/js/lines.js
@@ -17,69 +17,70 @@
 //      y: match id from this region
 //      color: each data type will have a corresponding color
 
-let regionData = [];
-let dataPoints = [];
-let regionArray = [];
-let population = [];
-const sColor = "#f68a41";
-const aColor = "#842037";
-const oColor = "#52a091";
-let defaultSort = "anemia";
+let regionData = []
+let dataPoints = []
+let regionArray = []
+let population = []
+const sColor = "#f68a41"
+const aColor = "#842037"
+const oColor = "#52a091"
+let defaultSort = 'anemia'
 
 let malnutrition = {
   stunting: {
     name: "Stunting",
     color: sColor,
-    col: 2,
+    col: 2
   },
   anemia: {
     name: "Anemia",
     color: aColor,
-    col: 3,
+    col: 3
   },
   overweight: {
     name: "Overweight or Obese",
     color: oColor,
-    col: 4,
-  },
-};
+    col: 4
+  }
+}
 
-const malnutritionList = Object.keys(malnutrition);
+const malnutritionList = Object.keys(malnutrition)
 
 const dropdownOptions = [
   { name: "Population (2018 Estimated)", value: "population" },
-  { name: "Region", value: "region" },
-];
+  { name: "Region", value: "region" }
+]
+
 
 Highcharts.data({
   // Load Data in from Google Sheets
-  googleSpreadsheetKey: "1sLlKirSAEv5QYBQa2LJlzWMNmF3HkNFnjBHbUAJ5RFU",
+  googleSpreadsheetKey: '1sLlKirSAEv5QYBQa2LJlzWMNmF3HkNFnjBHbUAJ5RFU',
   googleSpreadsheetWorksheet: 1,
   switchRowsAndColumns: true,
   parsed: function parsed(columns) {
     // Remove header row (first element in columns array)
-    columns.shift();
+    columns.shift()
 
     // default data sort
-    const defaultSortValue = malnutrition[defaultSort].col;
-    columns.sort((a, b) => b[defaultSortValue] - a[defaultSortValue]);
+    const defaultSortValue = malnutrition[defaultSort].col
+    columns.sort((a, b) => b[defaultSortValue] - a[defaultSortValue])
 
     // iterate over data
     columns.forEach((row, i) => {
       // name the columns
-      const population = row[1];
-      const region = row[0];
-      const stunting = row[2];
-      const anemia = row[3];
-      const overweight = row[4];
+      const population = row[1]
+      const region = row[0]
+      const stunting = row[2]
+      const anemia = row[3]
+      const overweight = row[4]
 
-      const percentages = [stunting, anemia, overweight];
+      const percentages = [stunting, anemia, overweight]
       // For each row, determine lowest percentage and assign that to x
-      const min = Math.min(...percentages);
+      const min = Math.min(...percentages)
       // For each row, determine highest percentage and assign that to x2
-      const max = Math.max(...percentages);
+      const max = Math.max(...percentages)
 
-      const y = i;
+      const y = i
 
       // Push row object into regionData array
       regionData.push({
@@ -87,37 +88,37 @@ Highcharts.data({
         x2: max,
         y,
         region,
-        color: "lightGray",
+        color: 'lightGray',
         anemia,
         stunting,
         overweight,
-        population,
-      });
+        population
+      })
       // For each row, push region to regionArray
-      regionArray.push(region);
+      regionArray.push(region)
 
       const tipGroup = {
-        stunting: {
-          name: "Stunting",
-          val: stunting,
-          color: sColor,
+        "stunting": {
+          "name": "Stunting",
+          'val': stunting,
+          'color': sColor
         },
-        anemia: {
-          name: "Anemia",
-          val: anemia,
-          color: aColor,
+        "anemia": {
+          "name": "Anemia",
+          "val": anemia,
+          'color': aColor
         },
-        overweight: {
-          name: "Overweight",
-          val: overweight,
-          color: oColor,
-        },
-      };
+        "overweight": {
+          "name": "Overweight",
+          "val": overweight,
+          'color': oColor
+        }
+      }
 
-      malnutritionList.forEach((item) => {
-        const { color, name, col } = malnutrition[item];
+      malnutritionList.forEach(item => {
+        const { color, name, col } = malnutrition[item]
 
-        let value = row[col];
+        let value = row[col]
 
         dataPoints.push({
           x: value,
@@ -126,100 +127,95 @@ Highcharts.data({
           name,
           population,
           region,
-          tipGroup,
-        });
-      });
-    });
-    populateSelect();
-    renderChart(regionData, dataPoints, regionArray);
-  },
-});
+          tipGroup
+        })
+      })
+    })
+    populateSelect()
+    renderChart(regionData, dataPoints, regionArray)
+  }
+})
 
 function populateSelect() {
   // Target the sort element
-  const datasets = document.getElementById("datasets");
+  const datasets = document.getElementById('datasets')
 
   // Add malnutritions to dropdownOptions array
   for (let value in malnutrition) {
     dropdownOptions.push({
       name: malnutrition[value].name,
-      value,
-    });
+      value
+    })
   }
 
   // Sort dropdown options
-  dropdownOptions.sort((a, b) => a.name.localeCompare(b.name));
+  dropdownOptions.sort((a, b) => a.name.localeCompare(b.name))
 
   // Create option elements for dropdown
   dropdownOptions.forEach((option, i) => {
-    const optionEl = document.createElement("option");
+    const optionEl = document.createElement("option")
     if (option.value === defaultSort) {
-      optionEl.selected = "selected";
+      optionEl.selected = 'selected'
     }
-    optionEl.value = option.value;
-    optionEl.text = option.name;
-    datasets.appendChild(optionEl);
-  });
+    optionEl.value = option.value
+    optionEl.text = option.name
+    datasets.appendChild(optionEl)
+  })
   datasets.onchange = function () {
-    regionArray = [];
+    regionArray = []
 
     // Sort the region data based on the selection
-    regionData = sortRegions(regionData, this.value);
+    regionData = sortRegions(regionData, this.value)
 
     // Update y value based on sort
     regionData.forEach((row, i) => {
-      row.y = i;
+      row.y = i
 
       // Push region to RegionArray
-      regionArray.push(row.region);
+      regionArray.push(row.region)
 
       // if the row region matches the dataPoints region, update y to i value
       dataPoints.forEach((dataRow, j) => {
         if (row.region === dataRow.region) {
-          dataRow.y = i;
+          dataRow.y = i
         }
-      });
-    });
-    const chart = Highcharts.chart("hcContainer", {});
-    chart.destroy();
-    renderChart(regionData, dataPoints, regionArray);
-  };
+      })
+    })
+    const chart = Highcharts.chart('hcContainer', {})
+    chart.destroy()
+    renderChart(regionData, dataPoints, regionArray)
+  }
 }
-
-console.log(regionData);
-console.log(dataPoints);
-console.log(regionArray);
 
 function sortRegions(dataToSort, sortBy) {
   if (sortBy === "region") {
-    return dataToSort.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+    return dataToSort.sort((a, b) => b[sortBy].localeCompare(a[sortBy]))
   }
-  return dataToSort.sort((a, b) => b[sortBy] - a[sortBy]);
+  return dataToSort.sort((a, b) => b[sortBy] - a[sortBy])
 }
 
 function renderChart(regionData, dataPoints, regionArray) {
   Highcharts.setOptions({
     lang: {
-      thousandsSep: ",",
-    },
-  });
+      thousandsSep: ","
+    }
+  })
 
-  Highcharts.chart("hcContainer", {
+  Highcharts.chart('hcContainer', {
     chart: {
-      height: "80%",
+      height: '80%'
     },
     exporting: {
       sourceWidth: 723,
       sourceHeight: 775,
       chartOptions: {
         title: {
-          text: "The Triple Burden of Malnutrition in Tanzania",
+          text: "The Triple Burden of Malnutrition in Tanzania"
         },
         subtitle: {
-          text:
-            "This chart shows the co-occurrence of three major types of malnutrition—overweight or obese, stunting, and anemia. The key population for stunting, or below average height for age, is children under 5. Women of reproductive age (15 to 49 years) are the key population for anemia and overweight or obese.",
-        },
-      },
+          text: "This chart shows the co-occurrence of three major types of malnutrition—overweight or obese, stunting, and anemia. The key population for stunting, or below average height for age, is children under 5. Women of reproductive age (15 to 49 years) are the key population for anemia and overweight or obese."
+        }
+      }
     },
     // Chart Title and Subtitle
     title: {
@@ -232,16 +228,15 @@ function renderChart(regionData, dataPoints, regionArray) {
     credits: {
       enabled: true,
       href: false,
-      text:
-        "CSIS Global Food Security Project | Source: Tanzania National Nutrition Survey 2018",
+      text: "CSIS Global Food Security Project | Source: Tanzania National Nutrition Survey 2018",
       position: {
-        y: 0,
-      },
+        y: 0
+      }
     },
     // Chart Legend
     legend: {
-      align: "left",
-      verticalAlign: "top",
+      align: 'left',
+      verticalAlign: 'top',
       useHTML: true,
       labelFormatter: function () {
         return (
@@ -254,24 +249,24 @@ function renderChart(regionData, dataPoints, regionArray) {
           '<span style="color:' +
           oColor +
           '">\u25CF</span> Overweight or Obese'
-        );
+        )
       },
       // remove default formatting
       symbolHeight: 0,
       symbolWidth: 0,
       symbolRadius: 0,
       itemStyle: {
-        cursor: "default",
-      },
+        cursor: 'default'
+      }
     },
     // X Axis
     xAxis: {
       labels: {
-        format: "{value}%",
+        format: '{value}%'
       },
       title: {
         enabled: true,
-        text: "Percent of Population",
+        text: 'Percent of Population',
       },
       maxPadding: 0.15, // extend axis to 60%
       minPadding: 0.3, // extend axis to 0%
@@ -282,85 +277,83 @@ function renderChart(regionData, dataPoints, regionArray) {
     // Y Axis
     yAxis: {
       title: {
-        enabled: false,
+        enabled: false
       },
       categories: regionArray,
-      gridLineColor: "transparent",
+      gridLineColor: 'transparent',
     },
     // Additional Plot Options
-    plotOptions: {
+    plotOptions:
+    {
       series: {
         borderWidth: 0,
         pointPadding: 0,
         groupPadding: 0,
         events: {
           legendItemClick: function () {
-            return false;
-          },
-        },
+            return false
+          }
+        }
       },
       xrange: {
-        enableMouseTracking: false,
+        enableMouseTracking: false
       },
       scatter: {
-        showInLegend: true,
-      },
+        showInLegend: true
+      }
     },
     tooltip: {
       useHTML: true,
       formatter: function () {
-        anemiaArr = this.point.tipGroup["anemia"];
-        stuntingArr = this.point.tipGroup["stunting"];
-        overweightArr = this.point.tipGroup["overweight"];
+        anemiaArr = this.point.tipGroup['anemia']
+        stuntingArr = this.point.tipGroup['stunting']
+        overweightArr = this.point.tipGroup['overweight']
         return (
-          "Region: <b>" +
+          'Region: <b>' +
           this.point.region +
-          "</b><br/>" +
-          "Population: <b>" +
+          '</b><br/>' +
+          'Population: <b>' +
           this.point.population.toLocaleString() +
-          "</b><br/>" +
+          '</b><br/>' +
           '<span style="color:' +
           anemiaArr.color +
           '">\u25CF </span>' +
           anemiaArr.name +
-          ": " +
+          ': ' +
           anemiaArr.val +
-          "%<br/>" +
+          '%<br/>' +
           '<span style="color:' +
           stuntingArr.color +
           '">\u25CF </span>' +
           stuntingArr.name +
-          ": " +
+          ': ' +
           stuntingArr.val +
-          "%<br/>" +
+          '%<br/>' +
           '<span style="color:' +
           overweightArr.color +
           '">\u25CF </span>' +
           overweightArr.name +
-          ": " +
+          ': ' +
           overweightArr.val +
-          "%"
-        );
+          '%'
+        )
       },
-      backgroundColor: "rgb(255, 255, 255)",
+      backgroundColor: 'rgb(255, 255, 255)'
     },
-    series: [
-      {
-        type: "xrange",
-        pointWidth: 2,
-        id: "main",
-        name: "Region",
-        data: regionData,
-        showInLegend: false,
+    series: [{
+      type: 'xrange',
+      pointWidth: 2,
+      id: 'main',
+      name: "Region",
+      data: regionData,
+      showInLegend: false
+    }, {
+      type: 'scatter',
+      linkedTo: 'main',
+      marker: {
+        radius: 3.5
       },
-      {
-        type: "scatter",
-        linkedTo: "main",
-        marker: {
-          radius: 3.5,
-        },
-        data: dataPoints,
-      },
-    ],
-  });
+      data: dataPoints,
+    }]
+  })
 }
