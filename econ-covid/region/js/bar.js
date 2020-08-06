ac7a1b984913663b7fdf2075c2b4a175149b33d8
Lindsay Urchyk <urchykli@gmail.com>
2020-08-06 11:32:54 -0400

    Three Highcharts for the Econ Program's Covid page (#64)
    * Simon Chair Covid support by region bar chart
    
    * Simon Chair Covid support by institution bar chart
    
    * Simon Chair Covid support by institution type pie chart
    
    * Econ program covid charts source
    
    * Econ covid pie chart data labels
    
    * Econ covid institutions bar chart data labels
    
    * Econ covid regions bar chart data labels
    
    * Econ covid bar chart percentages one decimal

---
+++ econ-covid/region/js/bar.js
@@ -0,0 +1,60 @@
+Highcharts.chart("hcContainer", {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: "1a0wC2PhRyHJ8HKz3BB1nCTcV1KUbR2DjnqWpPom6jDM",
+    googleSpreadsheetWorksheet: 1,
+  },
+  // General Chart Options
+  chart: {
+    type: "column",
+  },
+  // Chart Title and Subtitle
+  title: {
+    text: "Approved Support by Region",
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS Economics Program | Source: IFI press releases",
+  },
+  // Chart Legend
+  legend: {
+    enabled: false,
+    title: {
+      text:
+        '<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>',
+    },
+    align: "center",
+    verticalAlign: "bottom",
+    layout: "horizontal",
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "USD Billions",
+    },
+  },
+  tooltip: {
+    valueDecimals: 2,
+    valuePrefix: "$",
+    valueSuffix: " billion",
+  },
+  // Additional Plot Options
+  plotOptions: {
+    column: {
+      stacking: null, // Normal bar graph
+      // stacking: "normal", // Stacked bar graph
+      dataLabels: {
+        enabled: true,
+        formatter: function () {
+          return Highcharts.numberFormat(this.y, 1);
+        },
+      },
+    },
+    series: {
+      colorByPoint: true,
+      colors: ["#004165", "#0065A6", "#0A8672", "#7b7d7c", "#E5EFF6"],
+    },
+  },
+});
