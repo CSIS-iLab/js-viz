6f9729ca2520c1128724fb1f757b8f08f5f5076b
Lindsay Urchyk <urchykli@gmail.com>
2020-08-27 12:49:03 -0400

    Fix language in Historical Immunization chart

--- kenya-immunization-highcharts/immunization-coverage/js/lines.js
+++ kenya-immunization-highcharts/immunization-coverage/js/lines.js
@@ -1,62 +1,64 @@
 Highcharts.setOptions({
   lang: {
-    thousandsSep: ',',
-    numericSymbols: null
+    thousandsSep: ",",
+    numericSymbols: null,
   },
   chart: {
     style: {
-      fontFamily: "Roboto"
-    }
-  }
+      fontFamily: "Roboto",
+    },
+  },
 });
 
-Highcharts.seriesTypes.line.prototype.drawLegendSymbol= Highcharts.seriesTypes.area.prototype.drawLegendSymbol;  
+Highcharts.seriesTypes.line.prototype.drawLegendSymbol =
+  Highcharts.seriesTypes.area.prototype.drawLegendSymbol;
 
 Highcharts.chart("hcContainer", {
-
-
   // Load Data in from Google Sheets
   data: {
     googleSpreadsheetKey: "10q0ygaHPEL53tAr8Nu3I4bh-PykxOYRIyDpa0RXE3jg",
     googleSpreadsheetWorksheet: 2,
-    switchRowsAndColumns: true
+    switchRowsAndColumns: true,
   },
   // General Chart Options
   chart: {
-    zoomType: 'x',
-    type: 'line'
+    zoomType: "x",
+    type: "line",
   },
   colors: [
-    // Kenya 
-    '#F26522',
-    
-    // AFRO Region-Measles 1st Dose 
-    '#F5A623',
-    
+    // Kenya
+    "#F26522",
+
+    // AFRO Region-Measles 1st Dose
+    "#F5A623",
+
     // Countries in the same income group
-    '#47ACB1',
-  
+    "#47ACB1",
+
     // Global
-    '#8E6C89',
+    "#8E6C89",
   ],
   // Chart Title and Subtitle
   title: {
-    text: "<span style='font-size: 32px;'>Historical Immunization Coverage in Kenya</span>",
-    margin: 40
+    text:
+      "<span style='font-size: 32px;'>Historical Immunization Coverage in Kenya</span>",
+    margin: 40,
   },
   subtitle: {
-    text: "<span style='font-size: 16px;'>Hover over the map and toggle on and off the sidebar options to see how Kenya's national immunization coverage has varied over the last several decades, based on the coverage with the first dose for the DPT3 vaccine among 1-year-olds.</span>"
+    text:
+      "<span style='font-size: 16px;'>Hover over the map and toggle on and off the legend options to see how Kenya's national immunization coverage has varied over the last several decades, based on the coverage with the first dose for the DPT3 vaccine among 1-year-olds.</span>",
   },
   // Credits
   credits: {
     enabled: true,
     href: false,
-    text: "CSIS Global Health Policy Center | Source: WHO"
+    text: "CSIS Global Health Policy Center | Source: WHO",
   },
   // Chart Legend
   legend: {
     title: {
-      text: '<br><span style="font-size: 12px; color: #808080";>(Click to hide)</span>',
+      text:
+        '<br><span style="font-size: 12px; color: #808080";>(Click to hide)</span>',
     },
     margin: 40,
     align: "center",
@@ -68,24 +70,25 @@ Highcharts.chart("hcContainer", {
     max: 100,
     min: 0,
     title: {
-      text: '% Coverage Measles 1st Dose'
-    }
+      text: "% Coverage Measles 1st Dose",
+    },
   },
   // X Axis
   xAxis: {
-    tickInterval: 2
+    tickInterval: 2,
   },
   // Tooltip
   tooltip: {
-    valueSuffix: '%',
+    valueSuffix: "%",
     borderColor: "gray",
     shared: true,
     useHTML: true,
     style: {
-      lineHeight: '21px',
-      opacity: 0.8
+      lineHeight: "21px",
+      opacity: 0.8,
     },
-    headerFormat: '<span style="font-size: 12px"><b>{point.key}</b></span><br/>'
+    headerFormat:
+      '<span style="font-size: 12px"><b>{point.key}</b></span><br/>',
   },
   // Additional Plot Options
   plotOptions: {
@@ -93,15 +96,15 @@ Highcharts.chart("hcContainer", {
       marker: {
         enabled: false,
         symbol: "circle",
-        radius: 3
+        radius: 3,
       },
-      lineWidth: 2
-    }
+      lineWidth: 2,
+    },
   },
   series: [
-    { dashStyle: "solid"},
-    { dashStyle: "dash"},
-    { dashStyle: "dash"},
-    { dashStyle: "dash"}
-  ]
+    { dashStyle: "solid" },
+    { dashStyle: "dash" },
+    { dashStyle: "dash" },
+    { dashStyle: "dash" },
+  ],
 });
