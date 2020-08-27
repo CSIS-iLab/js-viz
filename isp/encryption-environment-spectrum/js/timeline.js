c4a17100297b75447be173b733ddfe867ea75db1
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-08-27 13:41:47 -0400

    second timeline created as instructed

---
+++ isp/encryption-environment-spectrum/js/timeline.js
@@ -0,0 +1,109 @@
+$(function () {
+  $(`#hcContainer`).highcharts({
+    // General Chart Options
+    chart: {
+      zoomType: "x",
+      type: "timeline",
+      height: "300px",
+    },
+    exporting: { enabled: true },
+    // Chart Title and Subtitle
+    title: {
+      text: "The Encryption Policy Environment Spectrum",
+    },
+    subtitle: {
+      text:
+        "Call-out boxes show the segments of the policy environment spectrum. Hover to see examples of actors and states that fall within that segment.",
+    },
+    // Accessibility
+    accessibility: {
+      screenReaderSection: {
+        beforeChartFormat:
+          "<h5>{chartTitle}</h5>" +
+          "<div>{typeDescription}</div>" +
+          "<div>{chartSubtitle}</div>" +
+          "<div>{chartLongdesc}</div>" +
+          "<div>{viewTableButton}</div>",
+      },
+      point: {
+        valueDescriptionFormat: "{index}. {point.label}. {point.description}.",
+      },
+    },
+    // Colors
+    colors: ["#3B4A68", "#3E5A8E", "#406AB2", "#427CDD", "#4185F3"],
+    // Hardcoded data
+    series: [
+      {
+        data: [
+          {
+            name: "Design Mandates",
+            description:
+              "<b>At-Risk Groups: </b>Egypt's LGBT community remains at-risk for persecution and targeting. Egyptian law restricts access to encryption by requiring government approval for companies and platform seeking to encrypt their products. With many digital platforms not secured, those in the LGBT community in Egypt eliminate or reduce their digital footprint as a result.<br /><b>Businesses and Organizations: </b>Communications regulations in India require that any encryption provider using greater than 40-bit encryption reach an agreement with the government and disclose encryption keys before operating. Given that 40-bit encryption is largely obsolete, services such as WhatsApp openly use 256-bit encryption keys and unrecoverable encryption. While WhatsApp has not yet been banned or forced to turn over its keys, it faces increasing pressure from the Indian government for failing to turn over data in response to law enforcement requests. ",
+          },
+          {
+            name: "Technology-neutral Access Mandates",
+            description:
+              "<b>Foreign Policy: </b>U.S. soldiers from the 82nd Airborne Division deployed to Iraq in 2020 were instructed to use Signal and Wickr on U.S. government cell phones due to concerns over privacy and security of communications from Iranian eavesdropping and possible influence over Iraqi network providers, telecoms, and intelligence services.<br /><b>Businesses and Organizations: </b>Since 2018, the United Kingdom and Australia have passed laws that arguably give their governments the authority to demand that companies maintain the capability to facilitate access to encrypted data. In 2020, United States lawmakers proposed legislation requiring companies to provide technical mechanisms to gain access to information protected via unrecoverable encryption.",
+          },
+          {
+            name: "User Decryption Mandates",
+            description:
+              "<b>Terrorists, Extremists, and Hate Groups: </b>Members of the Islamic State in France have used encrypted messenger apps with auto-delete and self-destruct feature to prevent French law enforcement from obtaining communications and attack planning data even if members are compelled to provide biometric keys for device access.<br /><b>Independent Voices: </b>Journalists increasingly turn to device encryption on both computers and smart phones to protect their information, sources, and reporting. However, journalists at U.S. border crossings may be pressured to open their devices for warrantless searches during secondary screenings.",
+          },
+          {
+            name: "Workarounds to Access Encrypted Communications",
+            description:
+              "<b>Terrorists, Extremists, and Hate Groups: </b>In 2017 news outlets reported that German police with lawful hacking authorities were able to hack into Telegram accounts of right-wing extremists, accessing their encrypted communication and logging new messages in real time.<br /><b>At-Risk Groups: </b>Toronto-based Citizen Lab cites numerous countries with histories of targeting at-risk groups and civil society have been found to use technology that provides a workaround to device and message encryption. When users click on a link that installs spyware on their mobile device, the spyware operator now has access to the trove of personal data and information on the user's phone.",
+          },
+          {
+            name: "No Access to Encrypted Communications",
+            description:
+              "<b>Foreign Policy: </b>Russian diplomats and intelligence officers operating in the United States use commercially available encryption platforms to mask communications from U.S. law enforcement unable to obtain the data from providers and technology firms.<br /><b>Independent Voices: </b>Following the public disclosure of U.S. government documents by Edward Snowden in 2013, many in the independent voices community became aware of the need to encrypt data and communications in the face of modern surveillance techniques. Shortly thereafter, secure messaging applications incorporated end-to-end encryption by default.",
+          },
+        ],
+      },
+    ],
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS",
+    },
+    // Chart Legend
+    legend: {
+      title: {
+        text: "",
+      },
+      align: "center",
+      verticalAlign: "bottom",
+      layout: "horizontal",
+    },
+    // X and Y Axis
+    xAxis: {
+      visible: false,
+    },
+    yAxis: {
+      visible: false,
+    },
+    // Tooltip
+    tooltip: {
+      headerFormat: "",
+      style: {
+        width: 350,
+      },
+    },
+    // Additional Plot Options
+    plotOptions: {
+      // timeline: {
+      //   stacking: null,
+      //   borderColor: "transparent",
+      //   groupPadding: 0.1,
+      //   pointPadding: 0.05,
+      //   // pointWidth: 33,
+      //   dataLabels: {
+      //     enabled: false
+      //   }
+      // }
+    },
+  });
+});
--- isp/encryption-policy-spectrum/js/timeline.js
+++ isp/encryption-policy-spectrum/js/timeline.js
@@ -37,33 +37,29 @@ $(function () {
         data: [
           {
             name: "Design Mandates",
-            label: "China, Russia, India",
             description:
-              "Design mandates are laws and regulations that require companies to build their products and services in specific ways to facilitate access to data. Some impose strict design mandates on companies outlining how their products and services should be built, including the use of specific types of encryption. Some seek to limit the strength of encryption available to users. Design mandates, often operationalized through certification, licensing, or testing regimes, can be built to ensure that only accessible encryption systems would be allowed in a country.",
+              "Design mandates are laws and regulations that require companies to build their products and services in specific ways to facilitate access to data. Some impose strict design mandates on companies outlining how their products and services should be built, including the use of specific types of encryption. Some seek to limit the strength of encryption available to users. Design mandates, often operationalized through certification, licensing, or testing regimes, can be built to ensure that only accessible encryption systems would be allowed in a country.<br>Example countries: China, Russia, India, etc.",
           },
           {
             name: "Technology-neutral Access Mandates",
-            label: "United Kingdom, Australia, France",
             description:
-              "Technology-neutral mandates refrain from specifying how technology providers must build their systems but instead require companies to provide decrypted data when asked by the government. While technology companies are left with the flexibility to decide how access is accomplished, the end effect is the same as in the case of design mandates: only providers who are willing and able to provide decrypted data to the government can operate legally in the country. Some countries have passed laws that could allow them to mandate recoverability but have yet to enact them, and the exact scope of those authorities remains unclear.",
+              "Technology-neutral mandates refrain from specifying how technology providers must build their systems but instead require companies to provide decrypted data when asked by the government. While technology companies are left with the flexibility to decide how access is accomplished, the end effect is the same as in the case of design mandates: only providers who are willing and able to provide decrypted data to the government can operate legally in the country. Some countries have passed laws that could allow them to mandate recoverability but have yet to enact them, and the exact scope of those authorities remains unclear.<br>Example countries: United Kingdom, Australia, France, etc.",
           },
           {
             name: "User Decryption Mandates",
-            label: "France, Australia",
             description:
-              "Some countries have addressed the challenge of encryption by requiring users to facilitate access to communications. Instead of compelling companies to facilitate access to their customers’ data, users may be compelled to disclose their private encryption keys, including passwords and biometric keys such as fingerprints or face scans. User mandates avoid imposing constraints on companies that provide encrypted products and services but do result in companies informing users that their data is being collected.",
+              "Some countries have addressed the challenge of encryption by requiring users to facilitate access to communications. Instead of compelling companies to facilitate access to their customers’ data, users may be compelled to disclose their private encryption keys, including passwords and biometric keys such as fingerprints or face scans. User mandates avoid imposing constraints on companies that provide encrypted products and services but do result in companies informing users that their data is being collected. <br>Example countries: France, Australia, etc.",
           },
           {
             name: "Workarounds to Access Encrypted Communications",
-            label: "Germany, United Kingdom, Sweden",
+            label: "Germany, United Kingdom, Sweden, etc.",
             description:
-              "Government use of workaroundSome governments use workarounds to access encrypted systems rather than restrict access to encryption. These workarounds range from lawful hacking and “man-in-the-middle” attacks to “ghost key” proposal, in which companies would surreptitiously add users to chats without notifying the other chatters. Some countries develop in-house capabilities to do lawful hacking and codebreaking operations. Others turn to vendors to buy hacking products and services, fueling a “gray market” of cyber companies whose activities tend to be in legal and ethical gray areas.s to access encrypted systems rather than restricting access to encryption.",
+              "Government use of workaroundSome governments use workarounds to access encrypted systems rather than restrict access to encryption. These workarounds range from lawful hacking and “man-in-the-middle” attacks to “ghost key” proposal, in which companies would surreptitiously add users to chats without notifying the other chatters. Some countries develop in-house capabilities to do lawful hacking and codebreaking operations. Others turn to vendors to buy hacking products and services, fueling a “gray market” of cyber companies whose activities tend to be in legal and ethical gray areas.s to access encrypted systems rather than restricting access to encryption. <br>Example countries: Germany, United Kingdom, Sweden, etc.",
           },
           {
             name: "No Access to Encrypted Communications",
-            label: "The United States",
             description:
-              "Some countries have no authorities in place to access encrypted communications. Countries may choose not to pursue authorities to access encrypted communications or may be unable to legislate accordingly. For advocates of encryption, this allows companies to maintain their privacy compact with users. For law enforcement, this environment poses the greatest challenge: agencies must rely primarily on unencrypted data sources and other methods of evidence gathering. Some countries may have the resources to support isolated attempts at technical workarounds, for most, certain types of data and communications will be effectively off-limits. ",
+              "Some countries have no authorities in place to access encrypted communications. Countries may choose not to pursue authorities to access encrypted communications or may be unable to legislate accordingly. For advocates of encryption, this allows companies to maintain their privacy compact with users. For law enforcement, this environment poses the greatest challenge: agencies must rely primarily on unencrypted data sources and other methods of evidence gathering. Some countries may have the resources to support isolated attempts at technical workarounds, for most, certain types of data and communications will be effectively off-limits. <br>Example countries: The United States, etc.",
           },
         ],
       },
