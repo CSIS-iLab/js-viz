a6446ce7ba60699b50d859afc3e3438aeb337335
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-09-16 11:57:43 -0400

    refactored network.js to have tooltip with description

--- ghpc-nutrition-covid/food-nutrition-and-covid19/js/network.js
+++ ghpc-nutrition-covid/food-nutrition-and-covid19/js/network.js
@@ -2,119 +2,140 @@ Highcharts.setOptions({
   chart: {
     style: {
       fontFamily: '"Open Sans", Arial, Helvetica, sans-serif',
-      fontSize: '16px'
+      fontSize: "16px",
     },
   },
 });
-
-Highcharts.addEvent(
-  Highcharts.Series,
-  'afterSetOptions',
-  function (e) {
-      var colors = Highcharts.getOptions().colors,
-          i = 0,
-          nodes = {};
-
-      if (
-          this instanceof Highcharts.seriesTypes.networkgraph &&
-          e.options.id === 'covid-tree'
-      ) {
-          e.options.data.forEach(function (link) {
-
-              if (link[0] === 'Nutrition & COVID-19') {
-                  nodes['Nutrition & COVID-19'] = {
-                      id: 'Nutrition & COVID-19',
-                      marker: {
-                          radius: 40
-                      }
-                  };
-                  nodes[link[1]] = {
-                      id: link[1],
-                      marker: {
-                          radius: 30
-                      },
-                      color: colors[i++]
-                  };
-              } else if (nodes[link[0]] && nodes[link[0]].color) {
-                  nodes[link[1]] = {
-                      id: link[1],
-                      color: nodes[link[0]].color
-                  };
-              }
-          });
-
-          e.options.nodes = Object.keys(nodes).map(function (id) {
-              return nodes[id];
-          });
-      }
-  }
-);
 
 $(function () {
   $(`#hcContainer`).highcharts({
     // General Chart Options
     chart: {
-      zoomType: 'x',
-      type: 'networkgraph'
+      zoomType: "x",
+      type: "networkgraph",
     },
     // Chart Title and Subtitle
     title: {
-      text: "Food Nutrition and COVID-19"
+      text: "Food Nutrition and COVID-19",
     },
     subtitle: {
-      text: "Click and drag to zoom in"
+      text: "Click and drag to zoom in",
     },
     // Credits
     credits: {
       enabled: true,
       href: false,
-      text: "CSIS Project Name | Source: NAME"
+      text: "CSIS Project Name | Source: NAME",
     },
     // Y Axis
     yAxis: {
-      title: { 
-        text: "Y Axis Title"
+      title: {
+        text: "Y Axis Title",
       },
     },
-    // Colors
-    colors: ["#004165", "#E86259", "#75BAA9", "#EDA27C", "#0064A6", "#4C8984"],
     // Additional Plot Options
-    plotOptions:
-    {
+    plotOptions: {
       networkgraph: {
-        keys: ['from', 'to'],
-        layoutAlgorithm: {
-            enableSimulation: true,
-            friction: -0.9
-        }
-    }
+        keys: ["from", "to"],
+      },
     },
     // Hard Coded Data
-    series: [{
-      dataLabels: {
+    series: [
+      {
+        dataLabels: {
           enabled: true,
-          linkFormat: ''
+          linkFormat: "",
+        },
+        data: [
+          {
+            from: "Nutrition & COVID-19",
+            to: "Disrupted Supply Chains",
+            tip:
+              "The pandemic’s disruption of supply chains has led to increased food insecurity and rising prices.",
+          },
+          {
+            from: "Nutrition & COVID-19",
+            to: "Interrupted Food Markets",
+            tip:
+              "Agricultural labor and markets have been disrupted due to travel restrictions and social distancing practices.",
+          },
+          {
+            from: "Nutrition & COVID-19",
+            to: "Loss of Income",
+            tip:
+              "The economic impacts of the pandemic, particularly job loss and disruption to the informal economy, have made purchasing food more difficult.",
+          },
+          {
+            from: "Nutrition & COVID-19",
+            to: "Poor Regulation",
+            tip:
+              "Lack of oversight has created greater potential for contamination or adulteration of street food, and price gouging in unstable markets.",
+          },
+          {
+            from: "Nutrition & COVID-19",
+            to: "Misinformation",
+            tip:
+              "False claims about certain foods and Covid-19, particularly about natural cures, have led to criminal activity selling these fake remedies.",
+          },
+        ],
+        // Node Color and Size
+        nodes: [
+          {
+            id: "Nutrition & COVID-19",
+            color: "#004165",
+            marker: {
+              radius: 30
+            }
+          },
+          {
+            id: "Disrupted Supply Chains",
+            color: "#E86259",
+            marker: {
+              radius: 30
+            }
+          },
+          {
+            id: "Interrupted Food Markets",
+            color: "#75BAA9",
+            marker: {
+              radius: 30
+            }
+          },
+          {
+            id: "Loss of Income",
+            color: "#EDA27C",
+            marker: {
+              radius: 30
+            }
+          },
+          {
+            id: "Poor Regulation",
+            color: "#0064A6",
+            marker: {
+              radius: 30
+            }
+          },
+          {
+            id: "Misinformation",
+            color: "#4C8984",
+            marker: {
+              radius: 30
+            }
+          },
+        ],
       },
-      id: 'covid-tree',
-      data: [
-      //   from: 'Nutrition & COVID-19',
-      //   to: 'Disrupted Supply Chains',
-      //   value: 'The pandemic’s disruption of supply chains has led to increased food insecurity and rising prices.'
-      // },
-      // {
-      //   from: 'Nutrition & COVID-19',
-      //   to: 'Interrupted Food Markets',
-      //   value: 'Agricultural labor and markets have been disrupted due to travel restrictions and social distancing practices.'
-      // },
-          ['Nutrition & COVID-19', 'Disrupted Supply Chains', 'The pandemic’s disruption of supply chains has led to increased food insecurity and rising prices.'],
-          ['Nutrition & COVID-19', 'Interrupted Food Markets', 'Agricultural labor and markets have been disrupted due to travel restrictions and social distancing practices.'],
-          ['Nutrition & COVID-19', 'Loss of Income'],
-          ['Nutrition & COVID-19', 'Poor Regulation'],
-          ['Nutrition & COVID-19', 'Misinformation'],
-          
-          // Leaves:
-          
-      ]
-  }],
-  })
-})
+    ],
+    // Tooltip
+    tooltip: {
+      formatter: function () {
+        var text = "Name: " + this.point.name + "<br>";
+
+        if (this.point.linksTo[0]) {
+          text += "Description: " + this.point.linksTo[0].tip;
+        }
+
+        return text;
+      },
+    },
+  });
+});
