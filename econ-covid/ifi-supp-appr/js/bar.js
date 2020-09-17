12388d70729651cfb505924f15472c34c116dad1
Jonathan Choi <55605733+Jonathan-J-Choi@users.noreply.github.com>
2020-09-17 15:53:06 -0400

    IFI covid support and IFI covid support/approval highcharts mocked up (#71)
    * IFI covid support and IFI covid support/approval highcharts mocked up
    
    * changes made as requested via github: colors, removal of zoom type, and removal of 'Legend Title' from title from both bar.js.  Changed CSIS Simon Chair from title in both index.html to CSIS Economics Program.
    
    * Format tooltip value for Econ covid charts
    
    * changes made according to request by Dylan: Tooltip to 1 decimal point. Legend changed to IMF + RFA Approved, IMF + RFA Disbursed, MDB Approved, MDB Disbursed.  Colors changed to make MDB bars and IMF bars similar to their counterparts. Legend changed to Approved and Disbursed on ifi-support.
    
    Co-authored-by: MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
    Co-authored-by: Lindsay Urchyk <urchykli@gmail.com>

---
+++ econ-covid/ifi-supp-appr/js/bar.js
@@ -0,0 +1,54 @@
+Highcharts.chart("hcContainer", {
+  // Load Data in from Google Sheets
+  data: {
+    googleSpreadsheetKey: "1a0wC2PhRyHJ8HKz3BB1nCTcV1KUbR2DjnqWpPom6jDM",
+    googleSpreadsheetWorksheet: 5,
+  },
+  // General Chart Options
+  chart: {
+    type: "column",
+  },
+  // Chart Title and Subtitle
+  title: {
+    text: "IFI Support by Country Income Group",
+  },
+  // Credits
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS Economics Program | Source: IFI press releases",
+  },
+  // Colors
+  colors: ["#004165", "#0065A6", "#0A8672", "#3ABCA6"],
+  // Chart Legend
+  legend: {
+    title: {
+      text:
+        '<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>',
+    },
+    align: "center",
+    verticalAlign: "bottom",
+    layout: "horizontal",
+  },
+  // Y Axis
+  yAxis: {
+    title: {
+      text: "USD Billions",
+    },
+  },
+  tooltip: {
+    valuePrefix: "$",
+    valueSuffix: " billion USD",
+    valueDecimals: 1,
+  },
+  // Additional Plot Options
+  plotOptions: {
+    column: {
+      stacking: null, // Normal bar graph
+      // stacking: "normal", // Stacked bar graph
+      dataLabels: {
+        enabled: false,
+      },
+    },
+  },
+});
