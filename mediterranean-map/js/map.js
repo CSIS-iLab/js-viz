dbcbe828573fecb1d14ad519dcba64f485b959a8
Serven Maraghi <maraghiserven@gmail.com>
2020-10-15 13:31:13 -0400

    Make program text modification requests

--- mediterranean-map/js/map.js
+++ mediterranean-map/js/map.js
@@ -81,18 +81,10 @@ function toggleActiveDataset(activeDataset) {
   dataStyle.setContent(setLayerStyle(activeDataset));
 }
 
-// polygon-fill: ramp([${activeDataset}], (#e5d0d0, #ddb6b5, #d49c9b, #c98281, #be6769, #cacdd3), (1, 2, 3, 5, 4, ), "=", category);
-
-// polygon-fill: ramp([${activeDataset}], (#cf9899, #c67f82, #bc676c, #b14d56, #a53041, #cacdd3), (1, 2, 3, 5, 4, ), "=", category);
-
-// polygon-fill: ramp([${activeDataset}],
-//   (#dab7b7, #d29d9e, #c98486,#bf6a6f,#b34f58), (1, 2, 3, 5, 4, ), "=", category);
-
 function setLayerStyle(activeDataset) {
-  // console.log(activeDataset);
   return `
     #layer {
-        polygon-fill: ramp([${activeDataset}], (#dab7b7, #d29d9e, #c98486, #bf6a6f, #b34f58), (1, 2, 3, 4, 5), "=", category);
+      polygon-fill: ramp([${activeDataset}], (#d0b3b4, #be9697, #ac7a7c, #9a5e62, #87424a), (1, 2, 3, 4, 5, ), "=", category);
     
       polygon-opacity: 0.9;
     }
@@ -101,19 +93,22 @@ function setLayerStyle(activeDataset) {
       line-color: #fff;
       line-opacity: 1;
     }
+    #layer::labels {
+      text-name: [country];
+      text-face-name: 'Lato Regular';
+      text-size: 14;
+      text-fill: #000;
+      text-label-position-tolerance: 0;
+      text-halo-radius: 1;
+      text-halo-fill: #ffffff;
+      text-dy: -3;
+      text-allow-overlap: false;
+      text-placement: point;
+      text-transform: uppercase;
+      text-placement-type: dummy;
+    }
   `;
 }
-
-// const legend = document.getElementById("legend");
-// const legendItems = legend.querySelectorAll("li");
-// function updateLegend(activeDataset) {
-//   let values = ["10", "1,000", ">10,000"];
-
-//   if (activeDataset === "total_deaths") {
-//     values = ["1", "100", ">1,000"];
-//   }
-
-// }
 
 function toggleActiveButton(activeButton) {
   if (activeButton === "risk") {
@@ -140,7 +135,7 @@ dataLayer.on(carto.layer.events.FEATURE_CLICKED, (event) => {
         ${data.country}
       </h2>
       <p class="popup__values">
-        Risk Type: 
+        Instability Markers: 
         <span>
           ${data.risk_type}
         </span>
@@ -164,7 +159,7 @@ dataLayer.on(carto.layer.events.FEATURE_CLICKED, (event) => {
         ${data.country}
       </h2>
       <p class="popup__values">
-        Risk Type: 
+        Influence Type: 
         <span>
           ${data.risk_type}
         </span>
