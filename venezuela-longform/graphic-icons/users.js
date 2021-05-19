5a6e2bd7749f2852e2a5bb4901c3faf44b62c854
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2021-05-19 13:53:54 -0400

    Title and text edits to both interactives.

--- venezuela-longform/graphic-icons/users.js
+++ venezuela-longform/graphic-icons/users.js
@@ -7,7 +7,7 @@ const users = [
   },
   {
     group: "wider-representation",
-    title: "A Process that Involves Women as Leaders and Partners",
+    title: "A Process That Involves Women as Leaders and Partners",
     desc:
       "Venezuelan women are disproportionally affected by the crisis but have not been adequately involved in past negotiations. This should be rectified as peace negotiation outcomes improve when women are actively involved.",
   },
@@ -15,23 +15,23 @@ const users = [
     group: "representation",
     title: "Authentic Representation of Venezuelan Society",
     desc:
-      "The negotiation process should represent a more complete spectrum of Venezuelan citizens, including civil society, smaller political parties, indigenous groups, trade unions, and Venezuelan migrants, among others.",
+      "The negotiation process should represent a more complete spectrum of Venezuelan citizens, including civil society, smaller political parties, Indigenous groups, trade unions, and Venezuelan migrants, among others.",
   },
   {
     group: "comb-int-pressure",
-    title: "United Opposition with a Fresh Narrative",
+    title: "United Opposition With a Fresh Narrative",
     desc:
-      "Before arriving at the negotiating table, Venezuelan opposition leaders should be united and collaborate with each other and any other pertinent players to develop a clear and renewed strategy that reframes negotiation as a sign of strength.",
+      "Before arriving at the negotiating table, Venezuelan opposition leaders should be united and collaborate with each other and any other pertinent players to develop a clear strategy that reframes negotiation as a sign of strength.",
   },
   {
     group: "timing-conditions",
-    title: "Timing & Conditions",
+    title: "Timing and Conditions",
     desc:
       "The regime needs to be under targeted internal and external pressure to be brought to the negotiating table in earnest. ",
   },
   {
     group: "structure-process",
-    title: "Stronger Structure & Process",
+    title: "Stronger Structure and Process",
     desc:
       "Venezuela needs a well-planned and professional negotiation structure with well-trained facilitators. The parties should be encouraged to jointly identify issues that merit being discussed in separate tables.",
   }
--- venezuela-longform/venezuela-sunburst/js/sunburst.js
+++ venezuela-longform/venezuela-sunburst/js/sunburst.js
@@ -4,21 +4,21 @@ var data = [
     parent: "",
     name: "Central Table",
     blurb:
-      "Norwegian team facilitates dialogue between opposition and regime delegations focused on macro-goal of free, fair, and transparent presidential elections",
+      "Norwegian team facilitates dialogue between opposition and regime delegations focused on the broad goal of free, fair, and transparent presidential elections",
   },
   {
     id: "1.1",
     parent: "0.0",
     name: "The Room Next Door",
     blurb:
-      "International table comprised of UN Ambassadors or other delegates from various countries, including the United States, Lima Group countries, the EU, Mexico, Panama, Argentina, Russia, Cuba, and China, all available for consultation with the Norwegian delegation. Possibly presence of multilateral organizations such as the UN and OAS.",
+      "International table comprised of UN ambassadors or other delegates from various countries, including the United States, Lima Group countries, the European Union, Mexico, Panama, Argentina, Russia, Cuba, and China, all available for consultation with the Norwegian delegation. Possibly presence of multilateral organizations such as the United Nations and OAS.",
   },
   {
     id: "1.2",
     parent: "0.0",
     name: "Civil Society Consultation Tables",
     blurb:
-      "Advisory group comprised of a diverse set of civil society leaders that has a direct and impartial channel of communication with both the central negotiating table and parallel tables. Could include human rights defenders, environmental activists, humanitarian relief workers, student leaders, and indigenous representatives, among others. ",
+      "Advisory group comprised of a diverse set of civil society leaders that has a direct and impartial channel of communication with both the central negotiating table and parallel tables. Could include human rights defenders, environmental activists, humanitarian relief workers, student leaders, and Indigenous representatives, among others. ",
     value: 100,
   },
   {
@@ -32,9 +32,9 @@ var data = [
   {
     id: "2.2",
     parent: "1.1",
-    name: "Public Health & Covid Vaccines",
+    name: "Public Health and Covid Vaccines",
     blurb:
-      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at  acquiring and distributing Covid-19 vaccines, personal protective equipment, and medicine. Mediated by PAHO or other international organization.",
+      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at  acquiring and distributing Covid-19 vaccines, personal protective equipment, and medicine. Mediated by Pan American Health Organization or other international organizations.",
     value: 25,
   },
   {
@@ -42,15 +42,15 @@ var data = [
     parent: "1.1",
     name: "Illegal Mining",
     blurb:
-      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at halting the illegal mining crisis in southern Venezuela and preventing further environmental devastation. Potential mediators include the UN or countries such as Costa Rica or Finland.",
+      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at halting the illegal mining crisis in southern Venezuela and preventing further environmental devastation. Potential mediators include the United Nations or countries such as Costa Rica and Finland.",
     value: 25,
   },
   {
     id: "2.4",
     parent: "1.1",
-    name: "Human Rights & Political Prisoners",
+    name: "Human Rights and Political Prisoners",
     blurb:
-      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at securing the release of political prisoners and opening access to continued investigations into the regime's human rights violations. Mediated by UNHRC or IACHR.",
+      "Specialized delegations from the opposition and the regime negotiate short-term trust-building partial agreements aimed at securing the release of political prisoners and opening access to continued investigations into the regime's human rights violations. Mediated by UN Human Rights Council or the Inter-American Commission on Human Rights.",
     value: 25,
   },
 ];
@@ -93,16 +93,13 @@ Highcharts.chart("hcContainer", {
   },
   // Chart Title and Subtitle
   title: {
-    text: "Interactive Title",
-  },
-  subtitle: {
-    text: "Click and drag to zoom in",
+    text: "The Negotiating Table",
   },
   // Credits
   credits: {
     enabled: true,
     href: false,
-    text: "CSIS Project Name | Source: NAME",
+    text: "",
   },
   tooltip: {
     useHTML: true,
