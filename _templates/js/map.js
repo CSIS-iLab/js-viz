fd2acb892233b2259830b0bf067c850c27214583
Serven Maraghi <maraghiserven@gmail.com>
2021-11-01 12:24:27 -0400

    Add carto template

---
+++ _templates/js/map.js
@@ -0,0 +1,67 @@
+var basemap = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/ilabmedia/ckvber5pm07gr14rq5v9ichdy/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw",
+  {}
+);
+
+var map = L.map("map", {
+  center: [0, 0],
+  zoom: 3,
+  maxZoom: 7,
+  scrollWheelZoom: true,
+  minZoom: 1,
+  zoomControl: true,
+  scrollWheelZoom: true,
+  layers: [basemap],
+  attributionControl: false,
+});
+
+const client = new carto.Client({
+  apiKey: "",
+  username: "csis",
+});
+
+const mapSource = new carto.source.SQL(`SELECT * FROM TABLENAME`);
+
+const mapStyle = new carto.style.CartoCSS(``);
+
+const mapLayer = new carto.layer.Layer(mapSource, mapStyle, {
+  featureOverColumns: [""],
+});
+
+client.addLayer(mapLayer);
+
+client.getLeafletLayer().bringToFront().addTo(map);
+
+const popup = L.popup({ closeButton: true });
+
+mapLayer.on(carto.layer.events.FEATURE_CLICKED, createPopup);
+
+function createPopup(event) {
+  popup.setLatLng(event.latLng);
+
+  if (!popup.isOpen()) {
+    var data = event.data;
+    console.log(event.data);
+    var content = "<div>";
+
+    content += `
+    <div class="popupHeaderStyle">
+      ${data.COLUMNAME}
+    </div>
+    <div class="popupEntryStyle">
+      ${data.COLUMNAME}
+    </div>
+    `;
+    popup.setContent("" + content);
+    popup.openOn(map);
+  }
+}
+
+L.control
+  .attribution({
+    position: "bottomright",
+  })
+  .setPrefix(
+    '<a href="https://www.csis.org/programs/PROGRAMNAME">CSIS PROGRAM</a>, <a href="https://leafletjs.com/">Leaflet</a>'
+  )
+  .addTo(map);
