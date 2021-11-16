7e5a9ecbbbb2d88303fb81e7a29c9950617020c1
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2021-11-16 16:11:02 -0500

    updated graph with Jose comments and colors

--- ghpc-kenya-pediatric-hiv/js/line.js
+++ ghpc-kenya-pediatric-hiv/js/line.js
@@ -1,13 +1,16 @@
-Highcharts.setOptions({
-  lang: {
-    thousandsSep: ','
-  }
-});
-
 Highcharts.chart('container', {
 
     title: {
-        text: 'New Pediatric (Ages 0-14) HIV Infections in Kenya'
+        text: 'New Pediatric (Ages 0-14) HIV Infections in Kenya',
+        style: {
+            fontSize: '1.5rem'
+        }
+    },
+    subtitle: {
+        text: "Hover over the graph to see how the number of new pediatric HIV infections (ages 0-14) has fallen year to year from 1996 to 2020.",
+        style: {
+            fontSize: '.9rem'
+        }
     },
     chart: {
         style: {
@@ -16,30 +19,33 @@ Highcharts.chart('container', {
     },
     data: {
         googleAPIKey: 'AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg',
-        googleSpreadsheetKey: '1HA3hEmlb-rrp1_WsN5abxflDUbEhu_u4Zi1s3aaLaAg'
+        googleSpreadsheetKey: '1HA3hEmlb-rrp1_WsN5abxflDUbEhu_u4Zi1s3aaLaAg',
+        googleSpreadsheetRange: "Sheet1"
     },
-
-    // subtitle: {
-    //     text: '*"Pediatric" refers to children ages 0-14'
-    // },
 
     yAxis: {
         title: {
-            text: 'Number of New Pediatric Infections'
+            text: 'Number of New Pediatric HIV Infections',
+            style: {
+                fontSize: '1rem'
+            }
         }
     },
 
     credits: {
-      text: 'CSIS Global Health Policy Center (GHPC) | Source: AIDSinfo, UNAIDS',
+      text: '<a href="https://www.csis.org/programs/global-health-policy-center">CSIS Global Health Policy Center (GHPC)</a> | Source: AIDSinfo, UNAIDS, 2020, <a href="https://aidsinfo.unaids.org/">https://aidsinfo.unaids.org/</a>',
       href: false
     },
 
     xAxis: {
         title: {
-           text: "Year"
+           text: "Year",
+            style: {
+                fontSize: '1rem'
+            }
         },
         accessibility: {
-            rangeDescription: 'Range: 1994 to 2020'
+            rangeDescription: 'Range: 1996 to 2020'
         }
     },
 
@@ -48,13 +54,27 @@ Highcharts.chart('container', {
     },
     plotOptions: {
         series: {
-            color: "#0065A4",
+            color: "#C44E47",
             label: false
         }
     },
     tooltip: {
-        pointFormat: '<b>{point.y:,.0f} Infections</b><br/>',
-        shared: true
+      style: {
+        fontSize: '.875rem'
+      },
+      headerFormat: "",
+      pointFormatter: function pointFormatter(e) {
+        return (
+          '<div><span style="font-size:18px;color:' +
+          this.color +
+          '">\u25CF </span><b>' +
+          this.x +
+          "</b><br/>" +
+          this.y.toLocaleString() +
+          " New HIV Infections" +
+          "</div>"
+        );
+      }
     }
 
 });
