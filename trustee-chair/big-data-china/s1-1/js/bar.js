3180822e92f6a6cb7d1345be60fc5086f780347b
jdromero88 <jdromerov88@gmail.com>
2022-01-25 10:59:45 -0500

    update font family

--- trustee-chair/big-data-china/s1-1/js/bar.js
+++ trustee-chair/big-data-china/s1-1/js/bar.js
@@ -1,103 +1,125 @@
-Highcharts.chart("hcContainer", {
-  // Load Data in from Google Sheets
-  data: {
-    googleAPIKey: 'AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg',
-    googleSpreadsheetKey: "1rPblPBVo7zD0u65uEGnaFCkVCBLt5t-ejEqh3q6xTWg",
-    googleSpreadsheetRange: "s1_1",
-  },
-  // General Chart Options
-  chart: {
-    type: "bar",
-    spacingBottom: 60
-  },
-  // Colors
-  colors: [
-    "#C52125", // Strongly Disagree
-    "#FD8A6D", // Disagree
-    "#FFF1B5", // Neutral
-    "#93BB84", // Agree
-    "#01846C", // Strongly Agree
-    "#C5C4C0" // refuse to answer
-  ],
-  // Chart Title and Subtitle
-  title: {
-    text: "The government should safeguard national sovereignty and territorial integrity as much as possible through diplomatic and economic means to avoid military conflicts.",
-    align: "left",
-    style: {
-      color: 'black',
-      fontSize: '20px',
-      fontWeight: 'bold'
-    }
-  },
-  subtitle: {
-    text: "% of respondents that believe ___",
-    align: "left",
-  },
-  // Credits
-  credits: {
-    enabled: true,
-    href: false,
-    text: "CSIS | Big Data China",
-    style: {
-      fontSize: '11px'
+var chart;
+$(document).ready(function() {
+  chart = new Highcharts.Chart({
+    // Load Data in from Google Sheets
+    data: {
+      googleAPIKey: 'AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg',
+      googleSpreadsheetKey: "1rPblPBVo7zD0u65uEGnaFCkVCBLt5t-ejEqh3q6xTWg",
+      googleSpreadsheetRange: "s1_1",
     },
-    position: {
-      y: -30
-    }
-  },
-  // Chart Legend
-  legend: {
-    align: "left",
-    x: -10,
-    verticalAlign: "top",
-    layout: "horizontal",
-    symbolRadius: 0,
-    itemStyle: {
-      color: '#333',
-      fontWeight: 'normal',
+    // General Chart Options
+    chart: {
+      type: "bar",
+      spacingBottom: 60,
+      events: {
+        load: function () {
+          var chart = this
+          console.log(chart.series)
+            $.each(chart.series, function (i, serie) {
+              console.log(serie);
+                $.each(serie.data, function (j, data) {
+                  
+                        if (data.yBottom - data.plotY < 15) data.dataLabel.hide();
+                    //
+                });
+            });
+        }
+      },
+      renderTo: 'hcContainer',
+      style: {
+        fontFamily: ['Source Sans Pro', 'sans-serif']
+      }
     },
-    labelFormatter: function () {
-      return this.name.slice(0, -2); // remove the last 2 characters of the string ' %'
-    }
-  },
-  // Y Axis
-  yAxis: {
+    // Colors
+    colors: [
+      "#C52125", // Strongly Disagree
+      "#FD8A6D", // Disagree
+      "#FFF1B5", // Neutral
+      "#93BB84", // Agree
+      "#01846C", // Strongly Agree
+      "#C5C4C0" // refuse to answer
+    ],
+    // Chart Title and Subtitle
     title: {
-      text: ""
+      text: "The government should safeguard national sovereignty and territorial integrity as much as possible through diplomatic and economic means to avoid military conflicts.",
+      align: "left",
+      style: {
+        color: 'black',
+        fontSize: '20px',
+        fontWeight: 'bold'
+      }
     },
-    max:100,
-    reversedStacks: false,
-    visible: false,
-  },
-  xAxis: {
-    type: "category"
-  },
-  // Tooltip
-  tooltip: {
-    headerFormat: '{point.key}<br/>',
-    pointFormatter: function () {      
-      return '<span style="font-size: 14px;color:' + this.color + '">\u25A0</span> ' + this.series.name.slice(0, -2) + ': <b> ' + this.y.toFixed(1) + '%</b><br/>'
+    subtitle: {
+      text: "% of respondents that believe ___",
+      align: "left",
     },
-    shared: true,
-  },
-  // Additional Plot Options
-  plotOptions: {
-    series: {
-      stacking: 'normal',
-      borderWidth: 0,
-      groupPadding: 0.1,
-      
-      dataLabels: {
-        align: 'left',
-        enabled: true,
-        formatter: function () {
-          return this.y.toFixed(1) + '%'
-        },
-        style: {
-          textOutline: 'none',
-          fontWeight: 'normal'
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS | Big Data China",
+      style: {
+        fontSize: '11px'
+      },
+      position: {
+        y: -30
+      }
+    },
+    // Chart Legend
+    legend: {
+      align: "left",
+      x: -10,
+      verticalAlign: "top",
+      layout: "horizontal",
+      symbolRadius: 0,
+      itemStyle: {
+        color: '#333',
+        fontWeight: 'normal',
+      },
+      labelFormatter: function () {
+        return this.name.slice(0, -2); // remove the last 2 characters of the string ' %'
+      }
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: ""
+      },
+      max:100,
+      reversedStacks: false,
+      visible: false,
+    },
+    xAxis: {
+      type: "category"
+    },
+    // Tooltip
+    tooltip: {
+      headerFormat: '{point.key}<br/>',
+      pointFormatter: function () {      
+        return '<span style="font-size: 14px;color:' + this.color + '">\u25A0</span> ' + this.series.name.slice(0, -2) + ': <b> ' + this.y.toFixed(1) + '%</b><br/>'
+      },
+      shared: true,
+    },
+    // Additional Plot Options
+    plotOptions: {
+      series: {
+        stacking: 'normal',
+        borderWidth: 0,
+        groupPadding: 0.1,
+        
+        dataLabels: {
+          align: 'left',
+          enabled: true,
+          formatter: function () {
+            return this.y.toFixed(1) + '%'
+          },
+          style: {
+            textOutline: 'none',
+            fontWeight: 'normal'
+          },
+          crop: false,
         }
       }
     }
-  }
+  });
 });
