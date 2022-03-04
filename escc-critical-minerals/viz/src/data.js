d5143d3d3c270e886a3a24a03368967667503290
Kai Elwood-Dieu <kaielwooddieu@gmail.com>
2022-03-04 16:12:23 -0500

    updated with serven's comments

--- escc-critical-minerals/viz/src/data.js
+++ escc-critical-minerals/viz/src/data.js
@@ -8,8 +8,6 @@ function parseData({ src }) {
     let [initialdata] = res;
     let allMinerals = initialdata.map(d => d.mineral);
     let minerals = [...new Set(allMinerals)];
-    const average = (inputArray, target) => inputArray.reduce((acc, country) => acc + country[target], 0)/inputArray.length;
-    let table = [];
 
     // correct to input number strings as ints, then filter to remove nan values
     let correcteddata = initialdata.map((d) => {
@@ -36,8 +34,6 @@ function parseData({ src }) {
       totalProduction: 0,
       totalReserves: 0
     }));
-    
-
 
     for(let x = 0; x < minerals.length; x++) {
       dataset[x].mineralData.forEach(d => {
@@ -48,42 +44,7 @@ function parseData({ src }) {
         d.percentReserves = ((d.reserves2020 / dataset[x].totalReserves)*100);
         d.percentProduction = ((d.production2020 / dataset[x].totalProduction)*100);
       })
-
-// ----------------- code below this line was for creating the average line --------------
-
-      // debugger;
-      // table[x] = dataset[x].mineralData.map(function (country){
-      //   let col1 = country.percentProduction - average(dataset[x].mineralData, "percentProduction");
-      //   let col2 = country.percentReserves - average(dataset[x].mineralData, "percentReserves");
-      //   let col3 = col1 * col2;
-      //   let col4 = col1 * col1;
-      //   return ([col1, col2, col3, col4])
-      // })
     }
-
-    // const trendline = (mineral, i) => {
-    //   console.log(mineral);
-    //   console.log(i);
-    //   let element1 = table[i];
-    //   console.log(element1);
-    //   let totalX = table[i].reduce((acc, nextEl) => acc + nextEl[2], 0);
-    //   let totalY = table[i].reduce((acc, nextEl) => acc + nextEl[3], 0);
-    //   let slope = totalX/totalY;
-    //   console.log(slope);
-    //   let averageX = average(mineral.mineralData, "percentProduction");
-    //   let averageY = average(mineral.mineralData, "percentReserves");
-    //   mineral.slope = slope;
-    //   mineral.intercept = []
-    //   // let dats2 = table[i].reduce((acc, table[i][3]) => acc + table[i][3], 0);
-    //   // return x
-    // }
-    // // debugger;
-    // dataset.map((mineral, index) => trendline(mineral, index))
-
-    // console.log("final data", dataset);
-
-    // ----------------------- end creating average line code -----------------
-
     return dataset
   })
   
