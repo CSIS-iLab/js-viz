fdc9c9d2de07f01507035aab80b0909cd0237e31
Jose Romero <jdromerov88@gmail.com>
2022-08-25 15:42:13 -0400

    Add sankey chart Venezuelan ended up in other countries

--- ghpc-venezuela-immunization-journey/migration/migration-corridors-for-venezuelan-migrants/js/sankey.js
+++ ghpc-venezuela-immunization-journey/migration/migration-corridors-for-venezuelan-migrants/js/sankey.js
@@ -7,27 +7,61 @@ Highcharts.setOptions({
 
 Highcharts.chart("hcContainer", {
   // Load Data in from Google Sheets
-  data: {
-    googleAPIKey: "AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg",
-    googleSpreadsheetKey: "12_ks76ZrqO3NcqmqlLtBi9ynpX0Zy9RvsWaprU47u4c",
-    googleSpreadsheetRange: "Migration-Corridors-for-Venezuelan-Migrants",
-  },
+  // data: {
+  //   googleAPIKey: "AIzaSyAImbihK2tiRewSFzuJTF_lcgPlGSr7zcg",
+  //   googleSpreadsheetKey: "12_ks76ZrqO3NcqmqlLtBi9ynpX0Zy9RvsWaprU47u4c",
+  //   googleSpreadsheetRange: "Migration-Corridors-for-Venezuelan-Migrants",
+  //   complete: function (data) {
+  //     console.log( data )
+  //     // data.series[0] = [{type: 'sankey'}]
+  //     return data
+  //   },
+  // },
+  series: [
+    {
+      colors: [
+        "#535353", // Venezuela
+        // "#FFC728", // Colombia
+        // "#8B7B5A", // Peru
+        // "#44C07B", // Chile
+        // "#0050A4", // Americas
+        // "#7D4391", // Ecuador
+      ],
+      keys: ["color", "from", "to", "weight"],
+      data: [
+        ["#FFC728", "Venezuela", "Colombia", 1780486],
+        ["#8B7B5A", "Venezuela", "Peru", 941889],
+        ["#44C07B", "Venezuela", "Chile", 523553],
+        ["#0050A4", "Venezuela", "United States of America", 505647],
+        ["#7D4391", "Venezuela", "Ecuador", 388861],
+      ],
+      type: "sankey",
+    },
+  ],
   // General Chart Options
   chart: {
-    type: "sankey",
+    // type: "sankey",
     spacingBottom: 60,
     style: {
       fontFamily: ["Source Sans Pro", "sans-serif"],
     },
   },
   // Colors
-  colors: ["#004C7B", "#0065A4", "#5E9DBF", "#BFD6DB"],
+  colors: [
+    // "#E53E3A", // Venezuela
+    "#535353", // Venezuela
+    "#FFC728", // Colombia
+    "#8B7B5A", // Peru
+    "#44C07B", // Chile
+    "#0050A4", // Americas
+    "#7D4391", // Ecuador
+  ],
   // Chart Title and Subtitle
   accessibility: {
-    description: "Migration Corridors for Venezuelan Migrants",
+    description: "Migration Corridors for Venezuelan Migrants (2020)",
   },
   title: {
-    text: "Migration Corridors for Venezuelan Migrants",
+    text: "Migration Corridors for Venezuelan Migrants (2020)",
     align: "left",
     style: {
       color: "black",
@@ -36,7 +70,7 @@ Highcharts.chart("hcContainer", {
     },
   },
   subtitle: {
-    text: "We need a subtitle?",
+    text: "Total number of migrants from Venezuela and which countries they ended up.",
     align: "left",
   },
   // Credits
@@ -52,55 +86,62 @@ Highcharts.chart("hcContainer", {
     },
   },
   // Chart Legend
-  legend: {
-    align: "left",
-    x: -10,
-    verticalAlign: "top",
-    layout: "horizontal",
-    symbolRadius: 0,
-    itemStyle: {
-      color: "#333",
-      fontWeight: "normal",
-    },
-    labelFormatter: function () {
-      return this.name;
-    },
-  },
+  // legend: {
+  //   align: "left",
+  //   x: -10,
+  //   verticalAlign: "top",
+  //   layout: "horizontal",
+  //   symbolRadius: 0,
+  //   itemStyle: {
+  //     color: "#333",
+  //     fontWeight: "normal",
+  //   },
+  //   labelFormatter: function () {
+  //     return this.name;
+  //   },
+  // },
   // Y Axis
-  yAxis: {
-    title: {
-      text: "People",
-    },
-    max: 6000000,
-    tickInterval: 1000000,
-    reversedStacks: false,
-    startOnTick: false,
-    endOnTick: false,
-    // visible: false,
-  },
-  xAxis: {
-    type: "year",
-    // max: 2017,
-    tickInterval: 1,
-    accessibility: {
-      rangeDescription: "Range: 2000 to 2021",
-    },
-    crosshair: true,
-  },
+  // yAxis: {
+  //   title: {
+  //     text: "People",
+  //   },
+  //   max: 6000000,
+  //   tickInterval: 1000000,
+  //   reversedStacks: false,
+  //   startOnTick: false,
+  //   endOnTick: false,
+  //   // visible: false,
+  // },
+  // xAxis: {
+  //   type: "year",
+  //   // max: 2017,
+  //   tickInterval: 1,
+  //   accessibility: {
+  //     rangeDescription: "Range: 2000 to 2021",
+  //   },
+  //   crosshair: true,
+  // },
   // Tooltip
   tooltip: {
-    headerFormat: "{point.key}<br/>",
-    pointFormatter: function () {
-      return (
-        '<span style="font-size: 14px;color:' +
-        this.color +
-        '">\u25A0</span> ' +
-        this.series.name +
-        ": <b> " +
-        new Intl.NumberFormat().format(this.y) +
-        "</b><br/>"
-      );
-    },
+    //   headerFormat: "{point.key}<br/>",
+    // pointFormatter: function () {
+    //   console.log("tooltip formatter", this);
+    //   point = this;
+    //   from = countries[point.from] || point.from;
+    //   to = countries[point.to] || point.to;
+    //   console.log(point.weight);
+    //   weight = new Intl.NumberFormat().format(point.weight);
+    //   return (
+    //     // '<span style="font-size: 14px;color:' +
+    //     // this.color +
+    //     // '">\u25A0</span> ' +
+    //     // this.series.name +
+    //     // ": <b> " +
+    //     // new Intl.NumberFormat().format(this.y) +
+    //     // "</b><br/>"
+    //     from + " → " + to + ": " + weight
+    //   );
+    // },
     shared: true,
     style: {
       fontSize: "14px",
