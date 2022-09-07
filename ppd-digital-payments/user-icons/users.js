a819fd88d1f8f2d7348c900911ca2dec590e3b5e
rockshellS <spicer.rachelle@gmail.com>
2022-09-07 16:47:26 -0400

    beginings og making an array of objects

--- ppd-digital-payments/user-icons/users.js
+++ ppd-digital-payments/user-icons/users.js
@@ -1,3 +1,22 @@
+// const icons = [
+//   {
+//     id: 'cash',
+//     format: "Cash",
+//     howWhereToReceive: "Post office, Agents, Bank",
+//   },
+//   {
+//     id: 'money-order',
+//     format: "Money Order",
+//     howWhereToReceive: "Post office, Agents, Bank",
+//   },
+//   {
+//     id: 'cash-card',
+//     format: "Cash Card",
+//     howWhereToReceive: "Post office, Agents, Bank",
+//   }
+// ]
+const icons = []
+
 let newURL = `https://content-sheets.googleapis.com/v4/spreadsheets/1H5JH0nsefgXAkGE6VRLUi1H50d0m4IeZBfJNZ-avEaI/values/channels-to-deliver-G2P?key=AIzaSyBXuQRRw4K4W8E4eGHoSFUSrK-ZwpD4Zz4&majorDimension=ROWS`;
 
 async function getData() {
@@ -18,34 +37,65 @@ async function getData() {
     iconId.push(element.toLowerCase().replaceAll(" ", "-"));
   });
 
+  const addDataArraysToIconArrray = () => {
+    icons.push(columnNames)
+    icons.push(rowNames)
+    
+  }
+  addDataArraysToIconArrray()
+
+  
+// const result = {}
+// icons.forEach((key, i) => result[key] = [columnNames[i]])
+// console.log(result)
+
+
+  const obj = icons[0].reduce((accumulator, value) => {
+    return {...accumulator, [value]: ''}
+  }, {})
+  console.log(obj)
+  console.log("icons", icons)
+
   function matchIds() {
     iconId.forEach((id) => {
-      const imgElement = document.querySelector(`#${id}`);
-
-      if (id === imgElement.id) {
-        tippy("#" + "imgElement.id", {
-          content:
-            "<div class='tip-header'><p>" +
-            columnNames +
-            " </p><p>hiiiiiiiiii </p></div>",
-          allowHTML: true,
-          arrow: "false",
-          interactive: "true",
-          placement: "auto",
-          trigger: "click",
-        });
-      }
+      tippy("#" + id, {
+         content: "<p> " + columnNames + "</p>",
+         allowHTML: true,
+         arrow: "false",
+         interactive: "true",
+         animation: 'fade',
+         placement: "auto",
+         
+       })
+     
+     
+    
     });
   }
 
   matchIds();
 
-  console.log("spreadsheet data (our original data.values): ", spreadsheetData);
-  console.log("column names: ", columnNames);
-  console.log("row names: ", rowNames);
-  console.log("icon IDs: ", iconId);
+  // console.log("spreadsheet data (our original data.values): ", spreadsheetData);
+  // console.log("column names: ", columnNames);
+  // console.log("row names: ", rowNames);
+  // console.log("icon IDs: ", iconId);
 
   return data;
 }
 
 getData();
+
+addTippy()
+
+function addTippy() {
+  icons.forEach( icon => {
+    tippy( "#" + icon.id, {
+      content: "<h1>" + icon.format + "</h1>",
+      allowHTML: true,
+      arrow: "false",
+      interactive: "true",
+      placement: "auto",
+      trigger: "click",
+    })
+  })
+}
