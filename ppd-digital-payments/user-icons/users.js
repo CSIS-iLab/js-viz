13568b8273ce2e523864cb674116c166bf08e08a
rockshellS <spicer.rachelle@gmail.com>
2022-09-08 13:01:47 -0400

    push indexes of arrays into own array

--- ppd-digital-payments/user-icons/users.js
+++ ppd-digital-payments/user-icons/users.js
@@ -26,35 +26,85 @@ async function getData() {
   let spreadsheetData = data;
   let rowData = spreadsheetData.values.slice(1);
   let columnNames = spreadsheetData.values[0];
-
-  let rowNames = [];
+  let formatValues = [];
+  let howWhereValues = []
+  let whereToSave = []
+  let pro = []
+  let cons = []
+  let examples = []
+console.log(rowData, "row data")
   rowData.forEach((element) => {
-    rowNames.push(element[0]);
+    // console.log(element, "element")
+    
+    formatValues.push(element[0]);
+  });
+  rowData.forEach((element) => {
+    howWhereValues.push(element[1]);
+  });
+  rowData.forEach((element) => {
+    whereToSave.push(element[2]);
+  });
+  rowData.forEach((element) => {
+    pro.push(element[3]);
+  });
+  rowData.forEach((element) => {
+    cons.push(element[4]);
+  });
+  rowData.forEach((element) => {
+    examples.push(element[5]);
   });
 
+
   let iconId = [];
-  rowNames.forEach((element) => {
+
+  formatValues.forEach((element) => {
     iconId.push(element.toLowerCase().replaceAll(" ", "-"));
   });
 
+
+
+
   const addDataArraysToIconArrray = () => {
     icons.push(columnNames)
-    icons.push(rowNames)
+    icons.push(formatValues)
+    icons.push(howWhereValues)
+    icons.push(whereToSave)
+    icons.push(pro)
+    icons.push(cons)
+    icons.push(examples)
     
   }
   addDataArraysToIconArrray()
 
+
+let newIcons = [{}]
+newIcons.push(columnNames[0])
+
+// console.log(newIcons, "new")
+
+columnNames.forEach(i => {
+  formatValues.map(j => {
+    icons.push({[i]:  j})
+  })
+})
+
+
+// const strings = icons.map((o) => JSON.stringify(o))
+// console.log(strings, "strings")
+// const newArray = strings.map((s) => JSON.parse(s))
+// console.log(newArray, "new Arr")
+// console.log(icons)
   
 // const result = {}
 // icons.forEach((key, i) => result[key] = [columnNames[i]])
 // console.log(result)
 
 
-  const obj = icons[0].reduce((accumulator, value) => {
-    return {...accumulator, [value]: ''}
-  }, {})
-  console.log(obj)
-  console.log("icons", icons)
+  // const obj = icons[0].reduce((accumulator, value) => {
+  //   return {...accumulator, [value]: ''}
+  // }, {})
+  // console.log(obj)
+  // console.log("icons", icons)
 
   function matchIds() {
     iconId.forEach((id) => {
@@ -76,8 +126,13 @@ async function getData() {
   matchIds();
 
   // console.log("spreadsheet data (our original data.values): ", spreadsheetData);
-  // console.log("column names: ", columnNames);
-  // console.log("row names: ", rowNames);
+  console.log("column names: ", columnNames);
+  // console.log("format values: ", formatValues);
+  // console.log("where/ how: ", howWhereValues);
+  // console.log("where to save: ", whereToSave);
+  // console.log("pro: ", pro);
+  // console.log("cons: ", cons);
+  // console.log("examples: ", examples);
   // console.log("icon IDs: ", iconId);
 
   return data;
