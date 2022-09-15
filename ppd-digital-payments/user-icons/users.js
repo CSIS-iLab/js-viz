a1490092bbcdf709a9a1e774281dade1f1edd1a7
rockshellS <spicer.rachelle@gmail.com>
2022-09-15 09:47:58 -0400

    cleaned up the code

--- ppd-digital-payments/user-icons/users.js
+++ ppd-digital-payments/user-icons/users.js
@@ -6,58 +6,41 @@ async function getData() {
   const response = await fetch(newURL);
   const data = await response.json();
 
-  // get our row names (cash, money order)
-  let rowNames = [];
+let rowNames = [];
   data.values.slice(1).forEach((element) => {
     rowNames.push(element[0]);
   });
 
-  // turn them into HTML Ids
-  let iconId = [];
+let iconId = [];
   rowNames.forEach((element) => {
     iconId.push(element.toLowerCase().replaceAll(" ", "-"));
   });
 
-  // create array to hold our objects
-
-  // figure out how tf the nested loop/2D array works
-  console.log("[i][j]");
-  console.log(data.values[0][0], "0 0");
-
-  console.log(data.values[1][1]);
-  console.log(data.values[2][0]);
-  console.log(data.values[3][0]);
-  console.log("------------");
-
-  for (let i = 1; i < 6; i++) {
-    
-    for (let j = 0; j < 6; j++) {
+for (let i = 1; i < 6; i++) {
+  for (let j = 0; j < 6; j++) {
       console.log(i, j, data.values[i][j]);
     }
   }
 
-  // build the object
-  const makeFinalObject = () => {
-    for (let i = 1; i < 6; i++) {
-      icons[i - 1] = {
-        id: iconId[i - 1],
-        format: data.values[i][0],
-        howWhereToReceive: data.values[i][1],
-        whereToSave: data.values[i][2],
-        pros: data.values[i][3],
-        cons: data.values[i][4],
-        examples: data.values[i][5],
-      };
-    }
-  };
+const makeFinalObject = () => {
+  for (let i = 1; i < 6; i++) {
+    icons[i - 1] = {
+      id: iconId[i - 1],
+      format: data.values[i][0],
+      howWhereToReceive: data.values[i][1],
+      whereToSave: data.values[i][2],
+      pros: data.values[i][3],
+      cons: data.values[i][4],
+      examples: data.values[i][5],
+    };
+  }
+};
 
-  makeFinalObject();
+makeFinalObject();
 
-  console.log(icons);
-
-  function addTippy() {
-    icons.forEach((icon) => {
-      tippy("#" + icon.id, {
+function addTippy() {
+  icons.forEach((icon) => {
+    tippy("#" + icon.id, {
         content: `  <div class="icon-container">
               <div class="tip-header">
                 <img src='icons/${icon.id}.png' alt='Icon' class='tip-icon' />
