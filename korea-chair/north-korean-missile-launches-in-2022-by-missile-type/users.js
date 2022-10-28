0a2dd3682c3e59d6b982eeaf45dfb009613d3424
Jose Romero <jdromerov88@gmail.com>
2022-10-28 17:55:27 -0400

    Create array of objects to use with tippy

--- korea-chair/north-korean-missile-launches-in-2022-by-missile-type/users.js
+++ korea-chair/north-korean-missile-launches-in-2022-by-missile-type/users.js
@@ -6,11 +6,25 @@ async function getData() {
   const RESPONSE = await fetch(URL)
   const DATA = await RESPONSE.json()
 
-  if (DATA) console.log(DATA)
-  // let rowNames = [];
-  // DATA.values.slice(1).forEach((element) => {
-  //   rowNames.push(element[0]);
-  // });
+  if (DATA) {
+    console.log(DATA)
+    let rowNames = []
+    const columnNames = DATA.values.shift()
+    const columnNamesLength = columnNames.length
+    console.log(columnNamesLength)
+    // console.log(columnNames)
+    console.log(DATA.values[0])
+    DATA.values.forEach((element) => {
+      ICONS.push({
+        date: element[0],
+        category: element[1],
+        id: element[2],
+      })
+      // console.log('im outsie now')
+    })
+    console.log(rowNames)
+    console.log(ICONS)
+  }
 
   // let iconId = [];
   // rowNames.forEach((element) => {
@@ -36,8 +50,30 @@ async function getData() {
   };
 
   if (missileChart) {
-    getMissileByID(DATA.values[2][2]);
+    ICONS.forEach( icon => {
+      // getMissileByID(icon.id)
+      console.log(icon.id);
+      tippy("#" + icon.id, {
+        content: `
+          <div class="icon-container">
+              <div class="tip-header">
+                <h3 class="header">${icon.id}</h3>
+              </div>
+              <h3 class="title">Category</h3>
+              <p class="info">${icon.category}</p>
+              <h3 class="title">Date launched:</h3>
+              <p class="info"> ${icon.date}</p>
+            </div>
+        `,
+        allowHTML: true,
+        arrow: true,
+        interactive: true,
+        placement: "auto",
+        followCursor: "initial",
+      });
+    })
   }
+
   function getMissileByID(id) {
     const missile = missileChart.contentDocument.querySelector('#' + id)
     console.log(missile)
@@ -46,34 +82,61 @@ async function getData() {
   // MAKE_FINAL_OBJECT()
 
   function addTippy() {
-    ICONS.forEach((icon) => {
-      tippy("#" + icon.id, {
-        content: `  <div class="icon-container">
-              <div class="tip-header">
-                <img src='icons/${icon.id}.png' alt='Icon' class='tip-icon' />
-                <h3 class="header">${icon.format}</h3>
+    console.log('inside addTippy');
+    if (missileChart) {
+      console.log('inside missileChart');
+      
+      ICONS.forEach((icon) => {
+        // getMissileByID(icon.id)
+        console.log('inside icon');
+        tippy("#" + icon.id, {
+          content: `
+            <div class="icon-container">
+                <div class="tip-header">
+                  <h3 class="header">${icon.id}</h3>
+                </div>
+                <h3 class="title">Category</h3>
+                <p class="info">${icon.category}</p>
+                <h3 class="title">Date launched:</h3>
+                <p class="info"> ${icon.date}</p>
               </div>
-              <h3 class="title">How/where to receive</h3>
-              <p class="info">${icon.howWhereToReceive}</p>
-              <h3 class="title">Where to save</h3>
-              <p class="info"> ${icon.whereToSave}</p>
-              <h3 class="title">Examples</h3>
-              <p class="info"> ${icon.examples}</p>
-              <h3 class="title">Cons</h3>
-              <p class="info"> ${icon.cons}</p>
-              <h3 class="title">Pros</h3>
-              <p class="info"> ${icon.pros}</p> 
-            </div>
           `,
-        allowHTML: true,
-        arrow: true,
-        interactive: true,
-        placement: "auto",
-        followCursor: "initial",
+          allowHTML: true,
+          arrow: true,
+          interactive: true,
+          placement: "auto",
+          followCursor: "initial",
+        });
       });
-    });
+    }
+    // ICONS.forEach((icon) => {
+    //   tippy("#" + icon.id, {
+    //     content: `  <div class="icon-container">
+    //           <div class="tip-header">
+    //             <img src='icons/${icon.id}.png' alt='Icon' class='tip-icon' />
+    //             <h3 class="header">${icon.format}</h3>
+    //           </div>
+    //           <h3 class="title">How/where to receive</h3>
+    //           <p class="info">${icon.howWhereToReceive}</p>
+    //           <h3 class="title">Where to save</h3>
+    //           <p class="info"> ${icon.whereToSave}</p>
+    //           <h3 class="title">Examples</h3>
+    //           <p class="info"> ${icon.examples}</p>
+    //           <h3 class="title">Cons</h3>
+    //           <p class="info"> ${icon.cons}</p>
+    //           <h3 class="title">Pros</h3>
+    //           <p class="info"> ${icon.pros}</p> 
+    //         </div>
+    //       `,
+    //     allowHTML: true,
+    //     arrow: true,
+    //     interactive: true,
+    //     placement: "auto",
+    //     followCursor: "initial",
+    //   });
+    // });
   }
-  // addTippy();
+  addTippy();
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
