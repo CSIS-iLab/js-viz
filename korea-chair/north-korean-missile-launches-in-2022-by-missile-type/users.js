73ce7a4dd13f7c8164c4f5563ddb842751846c38
Jose Romero <jdromerov88@gmail.com>
2022-11-15 10:49:45 -0500

    Clean up code KC missile chart

--- korea-chair/north-korean-missile-launches-in-2022-by-missile-type/users.js
+++ korea-chair/north-korean-missile-launches-in-2022-by-missile-type/users.js
@@ -1,38 +1,38 @@
 const ICONS = []
 const CATEGORIES = []
-let windowWidthSize = 0
 const sortedData = {}
-const URL = `https://content-sheets.googleapis.com/v4/spreadsheets/1YZ0D6cqMnk0kDCbtXcq2kp4hgUlGFCeUBnpjhbkgXAQ/values/Sheet1?key=AIzaSyBXuQRRw4K4W8E4eGHoSFUSrK-ZwpD4Zz4&majorDimension=ROWS`
+const URL = "https://content-sheets.googleapis.com/v4/spreadsheets/1YZ0D6cqMnk0kDCbtXcq2kp4hgUlGFCeUBnpjhbkgXAQ/values/Sheet1?key=AIzaSyBXuQRRw4K4W8E4eGHoSFUSrK-ZwpD4Zz4&majorDimension=ROWS"
+let windowWidthSize = 0
 
 async function getData() {
   windowWidthSize = window.innerWidth
-  const RESPONSE = await fetch(URL)
+  const RESPONSE = await fetch( URL )
   const DATA = await RESPONSE.json()
 
-  if (DATA) {
+  if ( DATA ) {
     const columnNames = DATA.values.shift()
-    DATA.values.forEach((element) => {
-      ICONS.push({
-        date: new Date(element[0]).toLocaleDateString("en-US"),
+    DATA.values.forEach( element  => {
+      ICONS.push( {
+        date: new Date( element[0] ).toLocaleDateString( "en-US" ),
         category: element[1],
         id: element[2],
         name: element[3],
-      })
-      CATEGORIES.push(element[1])
-    })
-    const categories = getCategories(CATEGORIES)
-    const dataFormatted = formatCategories('category')
+      } )
+      CATEGORIES.push( element[1] )
+    } )
+    getCategories( CATEGORIES )
+    formatCategories( 'category' )
   }
 
-  const wrapper = document.querySelector(".wrapper")
-  const wrapperMobile = document.querySelector(".wrapper-mobile")
-  const interactive = document.querySelector(".interactive")
-  const interactiveMobile = document.querySelector(".interactive-mobile")
+  const wrapper = document.querySelector( ".wrapper" )
+  const wrapperMobile = document.querySelector( ".wrapper-mobile" )
+  const interactive = document.querySelector( ".interactive" )
+  const interactiveMobile = document.querySelector( ".interactive-mobile" )
 
   function addTippy() {
-    if (windowWidthSize > 799) {
-        ICONS.forEach((icon) => {
-          tippy("#" + icon.id, {
+    if ( windowWidthSize > 499 ) {
+        ICONS.forEach( ( icon ) => {
+          tippy( "#" + icon.id, {
             // append to an Element
             appendTo: wrapper,
             content: `
@@ -45,84 +45,82 @@ async function getData() {
                 </div>
             `,
             allowHTML: true,
-            // trigger: "mouseenter",
             arrow: true,
             interactive: true,
             placement: "auto",
             followCursor: "initial",
-          })
-        })
-      interactiveMobile.classList.add("hide")
-      interactive.classList.remove("hide")
+          } )
+        } )
+      interactiveMobile.classList.add( "hide" )
+      interactive.classList.remove( "hide" )
     } else {
-      interactiveMobile.classList.remove("hide")
-      interactive.classList.add("hide")
+      interactiveMobile.classList.remove( "hide" )
+      interactive.classList.add( "hide" )
       const dates = []
-      for (let [key, value] of Object.entries(sortedData)) {
+      for ( let [missileName] of Object.entries( sortedData ) ) {
           const dates = []
-          sortedData[key].forEach( el => {
-            dates.push(el.date)
-          })
-          Object.assign(sortedData[key], { dates:  [...new Set(dates)]})
-          let string = ''
-          const datesLength = sortedData[key].dates.length
-          for (let index = 0; index < datesLength; index++) {
-            string += sortedData[key].dates[index] + '</br>'
+          sortedData[missileName].forEach( el => {
+            dates.push( el.date )
+          } )
+          Object.assign( sortedData[missileName], { dates:  [...new Set( dates )]} )
+          let datesLaunched = ''
+          const datesLength = sortedData[missileName].dates.length
+          for ( let index = 0; index < datesLength; index++ ) {
+            datesLaunched += sortedData[missileName].dates[index] + '</br>'
           }
-          tippy("#" + key, {
+          const title = ( datesLength > 1 ) ? 'Dates launched:' : 'Date launched:'
+
+          tippy("#" + missileName, {
             // append to an Element
             appendTo: wrapperMobile,
             content: `
               <div class="icon-container">
                 <div class="tip-header">
-                  <h3 class="header">${key}</h3>
+                  <h3 class="header">${missileName}</h3>
                 </div>
-                <h3 class="title">Date launched:</h3>
+                <h3 class="title">${title}</h3>
 
-                <p class="info"> ${string}</p>
+                <p class="info"> ${datesLaunched}</p>
               </div>
             `,
             allowHTML: true,
-            // trigger: "mouseenter",
             arrow: true,
             interactive: true,
             placement: "auto",
             followCursor: "initial",
           })
         }
-      // }
     }
   }
   addTippy()
 }
 
-function getCategories(categories) {
-  return [...new Set(categories)]
+function getCategories( categories ) {
+  return [...new Set( categories )]
 }
 
-function formatCategories(columnName) {
+function formatCategories( columnName ) {
   const iconsLength =  ICONS.length
-  const categoriesLength = columnName.length
 
   for (var i = 0; i < iconsLength; i++) {
     var object = ICONS[i]
 
-    if (Object.keys(sortedData).indexOf(object[columnName]) === -1) {
+    if ( Object.keys( sortedData ).indexOf( object[columnName] ) === -1 ) {
       sortedData[object[columnName]] = []
     }
 
-    sortedData[object[columnName]].push(object)
+    sortedData[object[columnName]].push( object )
   }
-  for (let key in sortedData) {
+  for ( let key in sortedData ) {
     sortedData[key].sort( ( a, b ) => new Date( b.date ) - new Date( a.date ) )
   }
   return sortedData
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener( "DOMContentLoaded", ( event ) => {
   getData()
 })
 
-window.addEventListener('resize', function(e) {
+window.addEventListener( 'resize', function( e ) {
   getData()
 }) 
