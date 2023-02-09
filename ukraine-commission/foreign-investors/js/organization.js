7cca7af2d1c787c928888ceaff2f91da5c24a583
Lindsay Urchyk <urchykli@gmail.com>
2023-02-09 10:11:44 -0500

    Ukraine commission 2 charts (#185)
    * foreign investors chart
    
    * Ukrainian private sector chart and styling for both charts
    
    * Ukraine Commission 2 chart fonts
    
    * Remove tooltips from Ukraine Commission 2 charts
    
    * Add hyphen in Ukrainian Private Sector chart

---
+++ ukraine-commission/foreign-investors/js/organization.js
@@ -0,0 +1,85 @@
+Highcharts.chart('container', {
+  chart: {
+    height: 600,
+    inverted: true,
+    style: {
+      fontFamily: "utopia-std",
+    },
+  },
+  title: {
+    text: ''
+  },
+  accessibility: {
+    point: {
+      descriptionFormatter: function(point) {
+        var nodeName = point.toNode.name,
+          nodeId = point.toNode.id,
+          nodeDesc = nodeName === nodeId ? nodeName : nodeName + ', ' + nodeId,
+          parentDesc = point.fromNode.id;
+        return point.index + '. ' + nodeDesc + ', reports to ' + parentDesc + '.';
+      }
+    }
+  },
+  series: [{
+    type: 'organization',
+    name: 'Highsoft',
+    keys: ['from', 'to'],
+    data: [
+      ['Foreign Investors', 'Multinational Corporations'],
+      ['Foreign Investors', 'Financial Institutions'],
+      ['Financial Institutions', 'Commercial Banks'],
+      ['Financial Institutions', 'Institutional Investors'],
+      ['Financial Institutions', 'Investment Funds'],
+    ],
+    levels: [{
+      level: 0,
+      color: '#FDF482',
+      dataLabels: {
+        color: 'black'
+      },
+      height: 25
+    }, {
+      level: 1,
+      color: '#32425C',
+      dataLabels: {
+        color: 'white'
+      },
+      height: 25
+    }, {
+      level: 2,
+      color: '#8FB5EA',
+      dataLabels: {
+        color: 'black'
+      }
+    }],
+    nodes: [{
+      id: 'Foreign Investors'
+    }, {
+      id: 'Multinational Corporations'
+    }, {
+      id: 'Financial Institutions',
+    }, {
+      id: 'Commercial Banks',
+    }, {
+      id: 'Institutional Investors',
+    }, {
+      id: 'Investment Funds',
+    }],
+    colorByPoint: false,
+    color: '#007ad0',
+    dataLabels: {
+      color: 'white'
+    },
+    borderColor: 'white',
+    nodeWidth: 65
+  }],
+  tooltip: {
+    enabled: false,
+    outside: true
+  },
+  exporting: {
+    allowHTML: true,
+    sourceWidth: 800,
+    sourceHeight: 600
+  }
+});
---
+++ ukraine-commission/ukrainian-private-sector/js/organization.js
@@ -0,0 +1,87 @@
+Highcharts.chart('container', {
+  chart: {
+    height: 600,
+    inverted: true,
+    style: {
+      fontFamily: "utopia-std",
+    },
+  },
+  title: {
+    text: ''
+  },
+  accessibility: {
+    point: {
+      descriptionFormatter: function (point) {
+        var nodeName = point.toNode.name,
+          nodeId = point.toNode.id,
+          nodeDesc = nodeName === nodeId ? nodeName : nodeName + ', ' + nodeId,
+          parentDesc = point.fromNode.id;
+        return point.index + '. ' + nodeDesc + ', reports to ' + parentDesc + '.';
+      }
+    }
+  },
+  series: [{
+    type: 'organization',
+    name: 'Highsoft',
+    keys: ['from', 'to'],
+    data: [
+      ['Ukrainian Private Sector', 'Small and Medium-Sized Enterprises (SMEs)'],
+      ['Ukrainian Private Sector', 'Local Financial Institutions'],
+      ['Local Financial Institutions', 'Investment Funds'],
+      ['Local Financial Institutions', 'Banks'],
+      ['Local Financial Institutions', 'Insurance Companies'],
+    ],
+    levels: [{
+      level: 0,
+      color: '#FDF482',
+      dataLabels: {
+        color: 'black'
+      },
+      height: 25
+    }, {
+      level: 1,
+      color: '#32425C',
+      dataLabels: {
+        color: 'white'
+      },
+      height: 25
+  }, {
+      level: 2,
+      color: '#8FB5EA',
+      dataLabels: {
+        color: 'black'
+      }
+    }],
+    nodes: [{
+      id: 'Ukrainian Private Sector'
+    }, {
+      id: 'Small and Medium-Sized Enterprises (SMEs)'
+    }, {
+      id: 'Ukrainian Private Sector',
+    }, {
+      id: 'Local Financial Institutions',
+    }, {
+      id: 'Investment Funds',
+    }, {
+      id: 'Banks',
+    }, {
+      id: 'Insurance Companies',
+    }],
+    colorByPoint: false,
+    color: '#007ad0',
+    dataLabels: {
+      color: 'white'
+    },
+    borderColor: 'white',
+    nodeWidth: 65
+  }],
+  tooltip: {
+    enabled: false,
+    outside: true
+  },
+  exporting: {
+    allowHTML: true,
+    sourceWidth: 800,
+    sourceHeight: 600
+  }
+});
