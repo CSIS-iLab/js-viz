6318d5cf92a106d4c0ab7dface923fcc841b4de4
Lindsay Urchyk <urchykli@gmail.com>
2023-02-14 13:01:24 -0500

    Update Netlify URL to point to production links

--- tnt-russia-btg-map/map/js/map-feb23.js
+++ tnt-russia-btg-map/map/js/map-feb23.js
@@ -27,7 +27,7 @@ const cartoSource = {
 function getImages() {
 	return new Promise((resolve, reject) => {
 		// let url = 'http://127.0.0.1:5503/tnt-russia-btg-map/map/js/markers.json';
-		let url = 'https://feat-tnt-russia-ukraine-map--csis-js-viz.netlify.app/tnt-russia-btg-map/map/js/markers.json';
+		let url = 'https://csis-js-viz.netlify.app/tnt-russia-btg-map/map/js/markers.json';
 		fetch(url)
 		.then(res => res.json())
 		.then((markers) => {
@@ -44,7 +44,7 @@ function getImages() {
 			for (let x in markers) {
 				x = x.toLowerCase()
 				// let fullUrl = "http://127.0.0.1:5503/tnt-russia-btg-map/map/images/" + x + ".svg";
-				let fullUrl = "https://feat-tnt-russia-ukraine-map--csis-js-viz.netlify.app/tnt-russia-btg-map/map/images/" + x + ".svg";
+				let fullUrl = "https://csis-js-viz.netlify.app/tnt-russia-btg-map/map/images/" + x + ".svg";
 				let filename2 = x.substring(x.lastIndexOf('/') + 1).replace(/\.[^/.]+$/, ""); // File name no ext
 				markerIcon = new IconBase({
 					iconUrl: fullUrl,
